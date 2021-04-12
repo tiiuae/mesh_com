c37345b2ffb2269541cbcd4f0e987310947ad5ee
Mika Joenpera <mika.joenpera@unikie.com>
2021-04-12 15:10:54 +0300

    common/tools/docker: more instructions to scripts and possibility give image name

--- common/tools/docker/ubuntu_16.04_for_openwrt/1_create_docker_image.sh
+++ common/tools/docker/ubuntu_16.04_for_openwrt/1_create_docker_image.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-docker build -t $1 .
+if [ $# -eq 0 ]
+  then
+      echo "Error:"
+      echo "Please give docker image tag name!"
+      echo "example. ./1_create_docker_image.sh <image tag name>"
+      echo
+      echo "example. ./1_create_docker_image.sh my_ubuntu_image"
+      exit
+fi
+docker build -t "$1" .
