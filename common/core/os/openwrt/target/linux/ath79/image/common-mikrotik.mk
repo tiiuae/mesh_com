ee13c1db31e3b8b3c595fc754de2814f21d8407e
Mika Joenpera <mika.joenpera@unikie.com>
2021-04-14 14:38:41 +0300

    openwrt clone removed.  Will be handled as submodule in the future

--- common/core/os/openwrt/target/linux/ath79/image/common-mikrotik.mk
+++
@@ -1,6 +0,0 @@
-define Device/mikrotik
-	DEVICE_VENDOR := MikroTik
-	LOADER_TYPE := elf
-	KERNEL := kernel-bin | append-dtb | lzma | loader-kernel
-	KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma | loader-kernel
-endef
