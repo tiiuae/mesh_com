1fb3b1eeed043850ce27c57e1c3a547967e474b1
Michael <michael@ssrc.tii.ae>
2021-05-03 08:39:25 +0400

    Add description for mesh-ap.sh

--- common/scripts/mesh-ap.sh
+++ common/scripts/mesh-ap.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
+
+################################################################################
+# Creates dhcp conf, configures interface ip, and vonfigures routing for mesh AP
+################################################################################
+# Configure dhcp
 cat <<EOF > /etc/dhcp/dhcpd.conf
 subnet $2.10.0.0 netmask 255.255.255.0 {
   range $2.10.0.2 $2.10.0.16;
@@ -6,10 +11,8 @@ subnet $2.10.0.0 netmask 255.255.255.0 {
   option routers $2.10.0.1;
 }
 EOF
-
-# give configuration to the passed interface
 ifconfig "$1" $2.10.0.1
 service isc-dhcp-server start
-sysctl -w net.ipv4.ip_forward=1
 # output all traffic from $2.10.0.0/24 to bat0 (i.e. mesh inf with a gw route)
+sysctl -w net.ipv4.ip_forward=1
 sudo iptables -t nat -A POSTROUTING -s $2.10.0.0/24 -o bat0 -j MASQUERADE
