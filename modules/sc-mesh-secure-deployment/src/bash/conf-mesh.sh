6c9f93b7a8963d1a96cc7c61d2ab7fe8d0ece2d6
Jaco <jaco@mail.de>
2021-09-08 14:52:29 +0400

    update path in bash scripts

--- modules/sc-mesh-secure-deployment/src/bash/conf-mesh.sh
+++ modules/sc-mesh-secure-deployment/src/bash/conf-mesh.sh
@@ -5,7 +5,7 @@ sc_path=$(pwd | cut -d'/' -f-5)
 mesh_interface=$1
 mesh_address=$2
 cp $meshcom_path/common/scripts/mesh-init.sh  /usr/sbin/.
-chmod 744 /usr/local/bin/mesh-init.sh
+chmod 744 /usr/sbin/mesh-init.sh
 cp $sc_path/services/initd/S90mesh /etc/init.d/.
 chmod 777 /etc/init.d/S90mesh
 /etc/init.d/S99mesh start $mesh_interface $address
