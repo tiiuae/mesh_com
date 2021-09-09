621e4c8590d4525bfc872c2774688750783b07cb
Jaco <jaco@mail.de>
2021-09-09 14:34:11 +0400

    fix missing path

--- modules/sc-mesh-secure-deployment/src/bash/conf-route.sh
+++ modules/sc-mesh-secure-deployment/src/bash/conf-route.sh
@@ -6,6 +6,6 @@ sc_path=$(pwd | cut -d'/' -f-5)
 route add default gw $gateway bat0
 cp $meshcom_path/common/scripts/mesh-default-gw.sh /usr/sbin/.
 chmod 744 /usr/sbin/mesh-default-gw.sh
-cp $sc_path/services/initd/S91defaultroute /etc/initd/.
-chmod 777 /etc/initd/S91defaultroute
+cp $sc_path/services/initd/S91defaultroute /etc/init.d/.
+chmod 777 /etc/init.d/S91defaultroute
 /etc/initd/S91defaultroute start $gateway
