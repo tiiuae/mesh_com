34cf4f3092240a189154f5a55f5939d1e6d14c31
Jaco <jaco@mail.de>
2021-09-15 11:10:29 +0400

    mesh-ibss.sh returned to its original state

--- common/scripts/mesh-ibss.sh
+++ common/scripts/mesh-ibss.sh
@@ -57,27 +57,27 @@ find_mesh_wifi_device()
 # 1      2    3      4        5     6       7      8         9         10
 # <mode> <ip> <mask> <AP MAC> <key> <essid> <freq> <txpower> <country> <interface>
 
-#echo "Solving wifi device name.."
-#if [[ -z "${10}" ]]; then
-#  rfkill unblock all
-#  # multiple wifi options --> can be detected as follows:
-#  # manufacturer 0x168c = Qualcomm
-#  # devices = 0x0034 0x003c 9462/988x
-#  #           0x003e        6174
-#  find_mesh_wifi_device 0x168c "0x003e 0x0034 0x003c"
-#
-#  if [ "$retval_phy" != "" ]; then
-#      phyname=$retval_phy
-#      wifidev=$retval_name
-#  else
-#      echo "ERROR! Can't find correct wifi device!"
-#      exit 1
-#  fi
-#else
-#  wifidev=${10}
-#  phyname=${11}
-#fi
-#echo "Found: $wifidev $phyname"
+echo "Solving wifi device name.."
+if [[ -z "${10}" ]]; then
+  rfkill unblock all
+  # multiple wifi options --> can be detected as follows:
+  # manufacturer 0x168c = Qualcomm
+  # devices = 0x0034 0x003c 9462/988x
+  #           0x003e        6174
+  find_mesh_wifi_device 0x168c "0x003e 0x0034 0x003c"
+
+  if [ "$retval_phy" != "" ]; then
+      phyname=$retval_phy
+      wifidev=$retval_name
+  else
+      echo "ERROR! Can't find correct wifi device!"
+      exit 1
+  fi
+else
+  wifidev=${10}
+  phyname=${11}
+fi
+echo "Found: $wifidev $phyname"
 
 case "$1" in
 
@@ -89,8 +89,6 @@ echo "sudo mesh $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11}"
           echo "check arguments..."
         help
       fi
-wifidev=${10}
-phyname=${11}
 
 cat <<EOF >/var/run/wpa_supplicant-adhoc.conf
 ctrl_interface=DIR=/var/run/wpa_supplicant
@@ -156,8 +154,6 @@ EOF
       # ensures wpa_supplicant doesn't start when this script is run as a process.
       # This is likely due to the interface not being up in time, and will
       # require some fiddling with the systemd startup order.
-      rm -fr /var/run/wpa_supplicant/"$wifidev"
-      rm -fr /var/run/wpa_supplicant/*
       if [[ -z "${10}" ]]; then
         wpa_supplicant -i "$wifidev" -c /var/run/wpa_supplicant-adhoc.conf -D nl80211 -C /var/run/wpa_supplicant/ -B
       else
@@ -229,7 +225,7 @@ EOF
 
 	    # set bat0 ipaddr
       if [ -z "$DRONE_DEVICE_ID" ]
-        then 
+        then
           # DRONE_DEVICE_ID not available set default
           ifconfig bat0 192.168.1.1 netmask 255.255.255.0
         else
@@ -265,4 +261,3 @@ off)
       help
       ;;
 esac
-
