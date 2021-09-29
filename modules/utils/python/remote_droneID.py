c5a63be142549e18be79b1e011cb4dcb80956870
Govind Singh <govind.sk85@gmail.com>
2021-09-29 20:03:29 +0530

    utils: broadcast drone remote id periodically
    Add drone remote id as vendor beacon ie periodically
    on sap interface.
    
    <dd bb dd cc 08 12 34 56 78 90> is the broadcast dri ID in the below beacon hex dump.
    
    nl80211: Set beacon (beacon_set=0)
    nl80211: Beacon tail - hexdump(len=135): 2a 01 04 32 04 30 48 60 6c 3b 02 51 00 2d 1a 0c 00
         1b ff ff 00 00 00 00 00 00 00 00 00 00 01 00 00 00 00 00 00 00 00 00 00 3d 16 01 00 00
         00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 7f 08 04 00 00 02 00 00 00 40
         dd 16 00 50 f2 01 01 00 00 50 f2 04 01 00 00 50 f2 04 01 00 00 50 f2 02 dd 18 00 50 f2
         02 01 01 01 00 03 a4 00 00 27 a4 00 00 42 43 5e 00 62 32 2f 00 dd bb dd cc 08 12 34 56 78 90
    
    Signed-off-by: Govind Singh <govind.sk85@gmail.com>
    Signed-off-by: Govind Singh <govind@ssrc.tii.ae>

--- modules/utils/python/remote_droneID.py
+++ modules/utils/python/remote_droneID.py
@@ -1,4 +1,7 @@
 import subprocess
+from time import sleep
+from threading import Thread
+import yaml
 
 #ROS node to convert to hex format
 #<element id><len><oui><vendor ie>
@@ -8,26 +11,45 @@ import subprocess
 #
 def prepare_vendor_ie(drone_id):
    length=len(drone_id);
-   print(length)
    #Add custom OUI
    oui="bbddcc"
    # 3 byte for OUI
    length=length/2 + 3
-   print(length)
    len_tag=format(int(length), '#04x')[2:]
-   print(len_tag)
    vendor_ie="dd" + oui + len_tag + drone_id
    return vendor_ie
 
 
 def update_vendor_ie(drone_id):
-    cmd="hostapd_cli SET vendor_elements " + drone_id
+    cmd="hostapd_cli SET vendor_elements " + str(drone_id)
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,shell=True)
     cmd="hostapd_cli DISABLE"
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,shell=True)
     cmd="hostapd_cli ENABLE"
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,shell=True)
 
+def dri_thread():
+    global dri_data_file
+    global dri_update_int
+    global debug
+
+    while True:
+        f = open(dri_data_file, 'r')
+        dri_data = f.read()
+        if debug:
+            print(dri_data)
+        f.close()
+        beacon_vendor_ie=prepare_vendor_ie(dri_data)
+        if debug:
+            print(beacon_vendor_ie)
+        update_vendor_ie(beacon_vendor_ie)
+        sleep(dri_update_int)
+
 if __name__=='__main__':
-    beacon_vendor_ie=prepare_vendor_ie("AB234566")
-    print(beacon_vendor_ie)
+    print('> Loading yaml conf... ')
+    conf = yaml.safe_load(open("dri.conf", 'r'))
+    debug = conf['debug']
+    dri_update_int = conf['dri_ie_update_interval']
+    dri_data_file = conf['dri_file_name']
+    Thread(target=dri_thread).start()
+
