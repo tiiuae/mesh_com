68a39962ac88dfdec9253dc0abc60e78f191428b
Govind Singh <govind.sk85@gmail.com>
2021-10-24 11:04:32 +0400

    mesh_com: minor cleanup and enhancements
    Restore ap.conf to original.
    Add pre-requisites for running docker in UBUNTU distro
    for unit testing.
    
    Signed-off-by: Govind Singh <govind.sk85@gmail.com>
    Signed-off-by: Govind Singh <govind@ssrc.tii.ae>

--- modules/utils/docker/set-docker.sh
+++ modules/utils/docker/set-docker.sh
@@ -2,6 +2,31 @@
 
 #set default gw (working on the lab)
 
+distro=$(lsb_release -a 2>/dev/null | grep "Distributor ID" | awk '{print $3}')
+echo $distro
+if [ "$distro" == "Ubuntu" ]; then
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+dockervar=$(ls /etc/init.d/ |grep docker)
+
+/etc/init.d/$dockervar stop
+/etc/init.d/$dockervar start
+else
 echo '> Type the gateway IP address...'
 read -p "- IP: " ip
 
@@ -31,4 +56,5 @@ adev=$(iw dev |wc -l)
 
 if [ "$adev" == 7 ]; then
 echo mmc1:0001:2 > /sys/bus/sdio/drivers/brcmfmac/bind
+fi
 fi
