9c67bdb2f4755d169e71f51f24180b81a31d7ccd
mandreoni <jaco@mail.de>
2022-01-18 13:49:48 +0400

    Added ip generation (add it to mesh_conf.conf) and some bugs fixed on prov-server.py added ip field on mesh_conf.conf

--- modules/sc-mesh-secure-deployment/src/authentication/prov-server.py
+++ modules/sc-mesh-secure-deployment/src/authentication/prov-server.py
@@ -10,7 +10,8 @@ import netifaces
 import paramiko
 import glob
 import argparse
-
+import random
+import ruamel.yaml
 
 # Construct the argument parser
 ap = argparse.ArgumentParser()
@@ -34,7 +35,7 @@ os.environ['GNUPGHOME'] = 'auth/'
 
 def create_table():
     '''
-    Function to create table of authenticated devices
+    Function to create table of authenticated devices.
     '''
     columns = ['ID', 'MAC', 'IP', 'PubKey_fpr']
     if not path.isfile('auth/dev.csv'):
@@ -47,15 +48,42 @@ def create_table():
 
 def update_table(info):
     '''
-    this function update the table with the node's info
+    this function update the table with the node's info.
+    Then, it updates mesh_conf_file with ip address.
+    Finally, it calls the transfer function.
     '''
     table = create_table()
     if info['ID'] not in set(table['ID']):
+        while info['IP'] in set(table['IP']):
+            info['IP'] = '10.0.0.' + str(generate_ip().pop())
         table = table.append(info, ignore_index=True)
-        table.to_csv('auth/dev.csv', mode='a', header=False, index=False)
-    elif table.loc[table['ID'] == info['ID']]['PubKey_fpr'] != info['PubKey_fpr']:
+        table.drop_duplicates(inplace=True)
+        table.to_csv('auth/dev.csv', index=False)
+    elif table.loc[table['ID'] == info['ID']]['PubKey_fpr'].all() != info['PubKey_fpr']:
         table = table.append(info, ignore_index=True)
-        table.to_csv('auth/dev.csv', mode='a', header=False, index=False)
+        table.drop_duplicates(inplace=True)
+        table.to_csv('auth/dev.csv', index=False)
+    update_conf_file(info['IP'])
+    file_keys = 'auth/' + info['ID'] + '.asc'
+    if info['ID'] != 'provServer':
+        file_keys = 'auth/node' + info['ID'] + '.asc'
+        sent = input("Would you like to send the certificates to a node? (yes/no): ")
+        if sent in ['yes', 'YES', 'Yes', 'y', 'Y']:
+            transfer(file_keys)
+
+
+def update_conf_file(ip):
+    '''
+    Update the mesh_conf file with the new ip address.
+    '''
+    file_name = '../mesh_com.conf'
+    config, ind, bsi = ruamel.yaml.util.load_yaml_guess_indent(open(file_name))
+    instances = config['server']['ubuntu']
+    instances['ip'] = ip
+    yaml = ruamel.yaml.YAML()
+    yaml.indent(mapping=ind, sequence=ind, offset=bsi)
+    with open('../mesh_conf.conf', 'w') as fp:
+        yaml.dump(config, fp)
 
 
 def exporting(ID, key, sign=True):
@@ -66,7 +94,7 @@ def exporting(ID, key, sign=True):
     '''
     ascii_armored_public_keys = gpg.export_keys(key)
     ascii_armored_private_keys = gpg.export_keys(key, True, expect_passphrase=False)
-    file_keys = 'auth/'+ID + '.asc'
+    file_keys = 'auth/' + ID + '.asc'
     if ID != 'provServer':
         file_keys = 'auth/node' + ID + '.asc'
         with open(file_keys, 'w') as f:
@@ -87,9 +115,6 @@ def exporting(ID, key, sign=True):
         print(ascii_armored_private_keys)
         print('\n---------------------------\n')
         print('Key pair stored in: ' + file_keys)
-        sent = input("Would you like to send the certificates to a node? (yes/no): ")
-        if sent in ['yes', 'YES', 'Yes', 'y', 'Y']:
-            transfer(file_keys)
     else:
         with open(file_keys, 'w') as f:
             f.write(ascii_armored_public_keys)
@@ -103,7 +128,13 @@ def clean_all():
     files = glob.glob('auth/*.asc')
     for fi in files:
         os.remove(fi)
-    os.remove('auth/dev.csv')
+    files = glob.glob('auth/*.conf.gpg')
+    for fi in files:
+        os.remove(fi)
+    try:
+        os.remove('auth/dev.csv')
+    except FileNotFoundError:
+        print('Nothing to clean')
     exit()
 
 
@@ -111,14 +142,14 @@ def exist(ID, verbose=True):
     '''
     Verify if the keys are presented in the keyring.
     If not, it will create new pair of keys.
-    If exist, it will verify if the keys are still valid.
+    If exists, it will verify if the keys are still valid.
     '''
     public_keys = gpg.list_keys()
     realID = ID + ' <' + ID + '>'
     if ID != 'provServer':
         realID = 'node' + ID + ' <' + ID + '>'
     if realID not in public_keys.uids:
-        return False
+        return False, False
     index = public_keys.uids.index(realID)
     valid = float(public_keys[index]['expires'])
     if valid > time.time():
@@ -126,15 +157,15 @@ def exist(ID, verbose=True):
             print('A key pair exist for node ' + ID + ' and it is valid until: ' +
                   str(datetime.datetime.utcfromtimestamp(valid)))
             exporting(ID, public_keys[index]['fingerprint'], False)
-        return True
+        return True, public_keys[index]['fingerprint']
     else:
         if verbose:
             print('A key pair exist for node ' + ID + ' but it expired on: ' +
                   str(datetime.datetime.utcfromtimestamp(valid)))
-        return False
+        return False, False
 
 
-def generate(input_data,  sign=True):
+def generate(input_data, sign=True):
     '''
     Generate new keys based on the input data
     '''
@@ -142,8 +173,8 @@ def generate(input_data,  sign=True):
     fingerprint = key.fingerprint
     ID = input_data.split('Name-Real: ')[1].split('\n')[0]
     if 'node' in ID:
-      ID = ID.split('node')[1]
-      print('Key pair generated with fingerprint ' + fingerprint)
+        ID = ID.split('node')[1]
+        print('Key pair generated with fingerprint ' + fingerprint)
     if not sign:
         exporting(ID, fingerprint, False)
     else:
@@ -182,7 +213,7 @@ def encrypt_conf(ID):
     recipients = [
         ids['fingerprint'] for ids in gpg.list_keys() if real in ids['uids']
     ]
-    output = ID+'_conf.conf.gpg'
+    output = ID + '_conf.conf.gpg'
     with open('../mesh_com.conf', 'rb') as f:
         gpg.encrypt_file(f, recipients=recipients,
                          output=output)
@@ -195,19 +226,24 @@ def transfer(FILE):
     '''
     server = input('Enter node IP address: ')
     username = 'root'
-    password = 'root' # assuming secure OS
+    password = 'root'  # assuming secure OS
     ssh = paramiko.SSHClient()
     ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
     ssh.connect(server, username=username, password=password)
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     sftp = ssh.open_sftp()
     only_node = FILE.split('auth/')[1]
-    sftp.put(FILE, 'hsm/'+only_node)
+    sftp.put(FILE, 'hsm/' + only_node)
     sftp.put('auth/provServer.asc', 'hsm/provServer.asc')
     encrypted_conf = encrypt_conf(FILE)
-    sftp.put(encrypted_conf, 'hsm/'+encrypted_conf.split('auth/')[1])
+    sftp.put(encrypted_conf, 'hsm/' + encrypted_conf.split('auth/')[1])
     sftp.close()
     ssh.close()
+
+
+def generate_ip():
+    ips = random.sample(range(2, 254), 2)
+    return ips
 
 
 if __name__ == "__main__":
@@ -216,7 +252,8 @@ if __name__ == "__main__":
     '''
     if args.clean:
         clean_all()
-    if not exist('provServer', False):
+    exists, fpr = exist('provServer', False)
+    if not exists:
         input_data = gpg.gen_key_input(name_real='provServer',
                                        passphrase='',
                                        no_protection=True,
@@ -224,11 +261,13 @@ if __name__ == "__main__":
                                        expire_date='1m'
                                        )
         fpr = generate(input_data, False)
-        mac = get_mac()  # only for server
-        info = {'ID': 'provServer', 'MAC': mac, 'IP': '0.0.0.0', 'PubKey_fpr': fpr}
-        update_table(info)
+
+    mac = get_mac()  # only for server
+    info = {'ID': 'provServer', 'MAC': mac, 'IP': '0.0.0.0', 'PubKey_fpr': fpr}
+    update_table(info)
     ID, mac = get_id()
-    if not exist(ID):
+    exists, fpr = exist(ID)
+    if not exists:
         input_data = gpg.gen_key_input(name_real='node' + str(ID),
                                        passphrase='',
                                        no_protection=True,
@@ -236,13 +275,13 @@ if __name__ == "__main__":
                                        expire_date='1d'
                                        )
         fpr = generate(input_data)
-        info = {'ID': ID, 'MAC': mac, 'IP': '0.0.0.0', 'PubKey_fpr': fpr}
-        update_table(info)
+    info = {'ID': ID, 'MAC': mac, 'IP': '10.0.0.' + str(generate_ip().pop()), 'PubKey_fpr': fpr}
+    update_table(info)
 
-        '''
-        TODO: 
-        * Managing keys: Delete in case they are compromised (manually?)
-        * How IP will be obtained?? provided by the server?
-        * Convert to der certificates?
-          
-        '''
+    '''
+    TODO: 
+    * Managing keys: Delete in case they are compromised (manually?)
+    * How IP will be obtained?? provided by the server? ->> done 
+    * Convert to der certificates?
+      
+    '''
