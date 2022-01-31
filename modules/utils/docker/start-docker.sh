cc425383cf77f6f08662ea94f3d43910b3d4a1f3
Govind Singh <govind.sk85@gmail.com>
2022-01-31 15:58:35 +0400

    mesh_com: Modify docker script for secOS docker
    Signed-off-by: Govind Singh <govind.sk85@gmail.com>
    Signed-off-by: Govind Singh <govind@ssrc.tii.ae>

--- modules/utils/docker/start-docker.sh
+++ modules/utils/docker/start-docker.sh
@@ -1,3 +1,18 @@
-docker build -t seccomms .
-docker run -it --privileged --net="host" --rm seccomms /bin/bash
-#docker run -it test /bin/bash
+#! /bin/bash
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+elif [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+fi
+if [ "$OS" == "Ubuntu" ]; then
+    docker build -t comms_vm .
+    docker run -it --privileged --net="host" --rm comms_vm /bin/bash
+elif [ "$OS" == "Buildroot" ]; then
+    # change rootfs location once its mounted in dedicated partation
+    docker import - comms_vm < /rootfs.tar
+    docker run --env EXECUTION_CTX='docker' -it --privileged --net="host" -v /opt:/opt comms_vm /bin/bash
+fi
