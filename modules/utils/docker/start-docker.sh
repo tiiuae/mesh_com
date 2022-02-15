35a04a34676e2f79d8659d03870efceef7787d8c
Govind Singh <govind.sk85@gmail.com>
2022-02-15 13:54:40 +0400

    mesh_com: Add backward compatibility with ubuntu docker
    ./start-docker.sh ubuntu/sec_os
    
    Signed-off-by: Govind Singh <govind.sk85@gmail.com>
    Signed-off-by: Govind Singh <govind@ssrc.tii.ae>

--- modules/utils/docker/start-docker.sh
+++ modules/utils/docker/start-docker.sh
@@ -1,4 +1,23 @@
 #! /bin/bash
+function help
+{
+    echo
+    echo "Usage: sudo ./start-docker.sh <docker_env"
+    echo "Parameters:"
+    echo "      <docker_env>"
+    echo "example:"
+    echo "sudo ./start-docker.sh ubuntu/sec_os"
+    exit
+}
+
+echo "sudo  start-docker.sh $1"
+    if [[ -z "$1" ]]; then
+        echo "check arguments..."
+        help
+    fi
+
+docker_exec_env=$1
+
 if [ -f /etc/os-release ]; then
     . /etc/os-release
     OS=$NAME
@@ -12,7 +31,12 @@ if [ "$OS" == "Ubuntu" ]; then
     docker build -t comms_vm .
     docker run -it --privileged --net="host" --rm comms_vm /bin/bash
 elif [ "$OS" == "Buildroot" ]; then
-    # change rootfs location once its mounted in dedicated partation
-    docker import - comms_vm < /rootfs.tar
-    docker run --env EXECUTION_CTX='docker' -it --privileged --net="host" -v /opt:/opt comms_vm /bin/bash
+    if [ "$docker_exec_env" == "sec_os" ]; then
+        # change rootfs location once its mounted in dedicated partation
+        docker import - comms_vm < /rootfs.tar
+        docker run --env EXECUTION_CTX='docker' -it --privileged --net="host" -v /opt:/opt comms_vm /bin/bash
+    elif [ "$docker_exec_env" == "ubuntu" ]; then
+        docker build -t comms_vm .
+        docker run -it --privileged --net="host" --rm comms_vm /bin/bash
+    fi
 fi
