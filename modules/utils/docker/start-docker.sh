a38e7fffd41f5f8a1d5bb02d2e330ee028a63f10
Govind Singh <govind.sk85@gmail.com>
2022-02-15 17:34:48 +0400

    mesh_com: create persist mesh container data partition with restart rule
    Signed-off-by: Govind Singh <govind.sk85@gmail.com>
    Signed-off-by: Govind Singh <govind@ssrc.tii.ae>

--- modules/utils/docker/start-docker.sh
+++ modules/utils/docker/start-docker.sh
@@ -32,9 +32,20 @@ if [ "$OS" == "Ubuntu" ]; then
     docker run -it --privileged --net="host" --rm comms_vm /bin/bash
 elif [ "$OS" == "Buildroot" ]; then
     if [ "$docker_exec_env" == "sec_os" ]; then
+        if [ ! -f "/opt/container-data/mesh" ]; then
+            # create persist mesh container data partation
+            # data/container-data/mesh
+            # ├── state.json
+            # └── wpa_supplicant_11s.conf
+            # └── wpa_supplicant_sta.conf
+            # └── wpa_supplicant_ap.conf
+            mkdir -p /opt/container-data/mesh
+        fi
         # change rootfs location once its mounted in dedicated partation
         docker import - comms_vm < /rootfs.tar
-        docker run --env EXECUTION_CTX='docker' -it --privileged --net="host" -v /opt:/opt comms_vm /bin/bash
+        docker run --env EXECUTION_CTX='docker' -it --privileged --net="host" -v /opt/container-data/mesh:/opt comms_vm /bin/bash
+        #Add restart policy of the container if it stops or device rebooted
+        docker update --restart unless-stopped comms_vm
     elif [ "$docker_exec_env" == "ubuntu" ]; then
         docker build -t comms_vm .
         docker run -it --privileged --net="host" --rm comms_vm /bin/bash
