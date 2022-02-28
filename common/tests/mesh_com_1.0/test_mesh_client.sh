fd066708f07d3bcfcc4009fcea87f5a5809a3290
Suhail <suhail@ssrc.tii.ae>
2022-02-28 10:20:08 +0400

    Added mesh client test case
    Signed-off-by: Suhail <suhail@ssrc.tii.ae>

---
+++ common/tests/mesh_com_1.0/test_mesh_client.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+source ./common/common.sh
+
+test_case="mesh_com v1.0 client functionality"
+
+description="mesh_com client Test Case"
+#######################################
+# Init
+# Globals:
+#  result
+#  device_list
+# Arguments:
+#######################################
+_init() {
+  _deinit
+  test_mesh_com_1.0_dependencies
+}
+
+#######################################
+# Test
+# Globals:
+#  result
+#  wifi dev
+#  channel_list
+# Arguments:
+#######################################
+_test() {
+  echo "$0, test called" | print_log
+
+  if ! [ "$result" -eq "$FAIL" ]; then
+    start_mesh_client
+  fi
+}
+
+#######################################
+# Result
+# Globals:
+#  result
+# Arguments:
+#######################################
+_result() {
+  echo "$0, result called" | print_log
+
+	SERVER_VALID=$(cat "$MESH_COM_ROOTDIR/modules/sc-mesh-secure-deployment/src/testclient.txt")
+	CLIENT_VALID=$(cat "$MESH_COM_ROOTDIR/modules/sc-mesh-secure-deployment/src/testclient1.txt")
+	CLIENT_MAC=$(cat "$MESH_COM_ROOTDIR/modules/sc-mesh-secure-deployment/src/testclientmac.txt")
+
+  if [[ "$SERVER_VALID" ]] && [[ "$CLIENT_VALID" ]] ; then
+    echo -e "Client is valid: $CLIENT_MAC" | print_log
+    result=$PASS
+  else
+    result=$FAIL
+  fi
+
+
+}
+
+#######################################
+# DeInit
+# Globals:
+#  device_list
+# Arguments:
+#######################################
+_deinit() {
+  echo "$0, deinit called" | print_log
+  #kill flask server
+  pid=$(netstat -tlnp | awk '/:5000 */ {split($NF,a,"/"); print a[1]}')
+  echo $pid
+  kill -9 $pid
+  killall wpa_supplicant
+  /etc/init.d/S50avahi-daemon stop
+  /etc/init.d/S30dbus stop
+}
+
+#######################################
+# main
+# Globals:
+#  result
+# Arguments:
+#######################################
+main() {
+  echo "### Test Case: $test_case" | print_log result
+  echo "### Test Description: $description" | print_log result
+
+  _init
+
+  if [ "$result" -eq "$FAIL" ]; then
+	echo "FAILED  _init: $test_case" | print_log result
+	exit 0
+  fi
+
+  _test
+
+  if [ "$result" -eq "$FAIL" ]; then
+	echo "FAILED  _test: $test_case" | print_log result
+	exit 0
+  fi
+
+   sleep 10
+  _result
+
+  if [ "$result" -eq "$FAIL" ]; then
+	echo "FAILED  : $test_case" | print_log result
+	exit 0
+  else
+	echo "PASSED  : $test_case" | print_log result
+  fi
+
+  _deinit
+}
+
+main
