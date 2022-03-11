ee2d2887cfb153e2a49644d4692c8e494cd51952
Aravind Raja <aravindaraja@ssrc.tii.ae>
2022-03-11 11:30:46 +0400

    Added comments

--- common/tests/security/deauth_attack.sh
+++ common/tests/security/deauth_attack.sh
@@ -4,9 +4,13 @@
 
 
 _init() {
+
+    # The functions verifies if the device is an ALFA capable of monitor mode. 
+	# Upon verfication it then puts the device into monitor mode to carry out atttacks	
   
-  echo "Init called"
+    echo "Init called"
 	
+	# Check if it is an ALFA device
 	addr=$(iw $interface info | grep addr | awk '{print $2}')
 	if [ ${add:0:8}=="00:c0:ca" ]; then
 		echo "Given Interface has a ALFA device, we can proced"
@@ -15,6 +19,7 @@ _init() {
 		exit 1
 	fi
 	
+	# Check if Monitor mode is already enabled, If not enable Monitor mode
 	_init_result
 	
 	if [ "$result" -eq "1" ]; then
@@ -26,6 +31,7 @@ _init() {
 		airmon-ng start $interface
 	fi
 	
+	# Check with Monitor mode initialization was sucessfull
 	_init_result
 	
 	if [ "$result" -eq "1" ]; then
@@ -39,6 +45,8 @@ _init() {
 
 _init_result() {
 
+	# Checks if the interface is in Monitor mode or now
+
 mode=$(iwconfig wlan0 | grep Mode | awk '{print $1}' | cut -d ":" -f2-)
 
 if [ $mode == "Monitor" ]; then
@@ -51,6 +59,8 @@ fi
 }
 
 _deinit() {
+
+	# Take the interface out of monitor mode
 
 airmon-ng stop $interface
 
@@ -68,7 +78,11 @@ channel=0
 
 bssid_list=()
 
-airodump=$(airodump-ng "$interface" -b a| grep WPA2 | head -n 10)
+# Scan the area for WPA3 networks
+
+airodump=$(airodump-ng "$interface" -b a| grep WPA3 | head -n 100)
+
+# Make a list of devices that would in the interested mesh comm
 
 while IFS= read -r line; do  
 
@@ -95,11 +109,12 @@ echo "List of the devices to be attacked"
 echo "${bssid_list[@]}"
 
 
-
+# Set the channel for sending attack packets
 echo "Launching airodump-ng on another terminal to set the channel"
 echo $channel
 xterm -e airodump-ng --channel "$channel" $interface & 
 
+# Loop and send Deauth packets to every device in list of targeted devices
 for value in ${bssid_list[@]};
 do	
     echo "Attacking device " $value " now "
@@ -113,11 +128,12 @@ done
 
 main(){
 
-interface=$1
-echo $interface
+
 _init
 
 _test
+
+
 }
 
 main "$@"
