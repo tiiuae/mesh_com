d6cc64b3777c9524e053ee8760585bde206d7046
Anshul Pandey <anshul@ssrc.tii.ae>
2022-04-04 12:09:20 +0400

    Uniformity in comments and corrected some typos

--- common/tests/mesh_com_1.0/test_mesh_iperf.sh
+++ common/tests/mesh_com_1.0/test_mesh_iperf.sh
@@ -19,43 +19,42 @@ _init() {
   count=`python neighbour_count.py`
   if [ $count -gt 0 ]
   then
-     echo "Neigbours Exist and there count is : $count"
+     echo "Neigbours exist and there count is : $count"
   else
-    echo "No Neighbours Exist, so exiting IPERF Test Setup"
+    echo "No neighbours exist, so exiting IPERF test setup"
     exit 1
   fi
 }
 
 _mesh_iperf_test() {
-  
-echo "Provide IP address of the device acting as Server over bat0 interface: "
+echo "Provide IP address of the device acting as server over Bat0 interface: "
 read server_ip
 ssh root@$server_ip 'iperf3 -s -D'
 #python server_iperf_script.py
-echo "IPERF Server is running in Background on $server_ip"
-echo "Please select the Type of Traffic"
-PS3="Select the Traffic Type: "
+echo "IPERF server is running in background on $server_ip"
+echo "Please select the type of traffic"
+PS3="Select the traffic type: "
 select opt in Voice  Video  BestEffort  Background quit; do
 
 case $opt in
     Voice)
       TOS=0xC0
-      echo "TOS value is 0xC0"
+      echo "Predefined TOS value is 0xC0"
       break
       ;;
     Video)
       TOS=0x80
-      echo "TOS value is 0x80"
+      echo "Predefined TOS value is 0x80"
       break
       ;;
     BestEffort)
       TOS=0x00
-      echo "TOS value is 0100"
+      echo "Predefined TOS value is 0100"
       break
       ;;
     Background)
       TOS=0x40
-      echo "TOS value is 0x40"
+      echo "Predefined TOS value is 0x40"
       break
       ;;
     quit)
@@ -73,13 +72,13 @@ select opt in UDP  TCP  quit; do
 
   case $opt in
     UDP)
-      echo "Generating Stats for UDP Case-----------PLease Wait................................."
+      echo "Generating stats for UDP case-----------please wait................................."
       udp=1
       iperf3 -u -c $server_ip  -b 1M -S $TOS -i 1 > iperf_test_result_udp.txt
       break
       ;;
     TCP)
-      echo "Generating Stats for TCP Case---------Please Wait.................................."
+      echo "Generating stats for TCP case---------please wait.................................."
       tcp=1
       iperf3 -c $server_ip -S $TOS -i 1 > iperf_test_result_tcp.txt
       break
@@ -92,22 +91,19 @@ select opt in UDP  TCP  quit; do
       ;;
   esac
 done
-
 }
 
 _result() {
      if [ $udp == '1' ] 
      then
-      echo " IPERF Test REsult for UDP Protocols..........."
+      echo " IPERF test result under  UDP protocol..........."
       awk -Wi -F'[ -]+' '/sec/{print " [Interval(seconds); Transfer Rate( MBytes); Throughput(Mbps); Jitter(ms); Loss Datagram/Total Datagram]= "$3"-"$4" "$6"  "$8"  "$10"  "$12}' iperf_test_result_udp.txt | tail -n 1
      fi
-  
      if [ $tcp == '1' ]
      then
-      echo " IPERF Test Result for TCP Protocol..........."
+      echo " IPERF test result under  TCP protocol..........."
       awk -Wi -F'[ -]+' '/sec/{print " [Interval(seconds); Transfer Rate( MBytes); Throughput(Mbps); Jitter(ms); Loss Datagram/Total Datagram]= "$3"-"$4" "$6"  "$8}' iperf_test_result_tcp.txt | tail -n 1
      fi
-    
     udp=0
     tcp=0
 }
@@ -116,7 +112,7 @@ _result() {
 
 _deinit() {
    ssh root@$server_ip 'killall -9  iperf3'
-   echo "IPERF SERVER $server_ip killed"
+   echo "IPERF server  $server_ip killed"
 }
 
 main() {
