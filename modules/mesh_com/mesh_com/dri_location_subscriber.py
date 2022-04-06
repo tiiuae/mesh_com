c4ad83e453ed29c71f993e18cb9e365ac61ccdf3
Govind Singh <govind.sk85@gmail.com>
2022-04-06 14:58:10 +0000

    mesh_com: Fix gps loc subscriber event registration and parsing
    Signed-off-by: Govind Singh <govind.sk85@gmail.com>

--- modules/mesh_com/mesh_com/dri_location_subscriber.py
+++ modules/mesh_com/mesh_com/dri_location_subscriber.py
@@ -18,19 +18,13 @@ class DRILocSubscriber(Node):
             depth=1,
             reliability=QoSReliabilityPolicy.RELIABLE,
             durability=QoSDurabilityPolicy.TRANSIENT_LOCAL)
-        # if __MOCAP__:
-        #     self.subscription = self.create_subscription(
-        #         String,
-        #         'uaeci01/fmu/sensor_gps/in',
-        #         self.listener_callback,
-        #         qos)
-        # else:
         self.subscription = self.create_subscription(
-            String,
-            'fmu/vehicle_gps_position/out',
+            SensorGps,
+            #Fix me
+            '/uae05/fmu/sensor_gps/in',
             self.listener_callback,
-            qos)
-        self.subscription  # prevent unused variable warning
+            10)
+        #self.subscription  # prevent unused variable warning
         self.HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
         self.PORT = 33222  # Port to listen on (non-privileged ports are > 1023)
         self.dri_loc_socket = socket.socket()  # to remove python warning
@@ -43,22 +37,25 @@ class DRILocSubscriber(Node):
         self.dri_loc_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         self.dri_loc_socket.settimeout(1)
 
-    def extract_loc_data(self):
-        # https://github.com/PX4/px4_msgs/blob/master/msg/SensorGps.msg
-        pass
-
     def listener_callback(self, msg):
         self.get_logger().info('listener_callback')
         self.destroy_timer(self.backup_timer)  # resend fresh data only
-        self.get_logger().info('mesh dri location subscriber: "%s"' % msg.data)
+        #self.get_logger().info('mesh dri location subscriber: "%s"' % msg)
+        # refer to https://github.com/PX4/px4_msgs/blob/master/msg/SensorGps.msg
+        gps_msg=(str(msg.timestamp) + ';' + str(msg.device_id) + ';' + str(msg.lat) + ';' + str(msg.lon) + ';' + str(msg.alt) + ';' +
+                str(msg.alt_ellipsoid) + ';' + str(msg.s_variance_m_s) + ';' + str(msg.c_variance_rad) + ';' + str(msg.fix_type) + ';' + str(msg.eph) + ';' +
+                str(msg.epv) + ';' + str(msg.hdop) + ';' + str(msg.vdop) + ';' + str(msg.jamming_indicator) + ';' + str(msg.jamming_state) + ';' +
+                str(msg.epv) + ';' + str(msg.hdop) + ';' + str(msg.vdop) + ';' + str(msg.jamming_indicator) + ';' + str(msg.jamming_state) + ';' +
+                str(msg.vel_m_s) + ';' + str(msg.vel_n_m_s) + ';' + str(msg.vel_e_m_s) + ';' + str(msg.vel_d_m_s) + ';' + str(msg.timestamp_time_relative) + ';' +
+                str(msg.time_utc_usec) + ';' + str(msg.satellites_used) + ';' + str(msg.heading) + ';' + str(msg.heading_offset) + ';' + str(msg.heading_accuracy) + ';')
         try:
-            if msg.data:
+            if gps_msg:
                 self.setup_socket()
                 self.dri_loc_socket.connect((self.HOST, self.PORT))
-                send_msg(self.dri_loc_socket, str.encode(msg.data))
+                send_msg(self.dri_loc_socket, str.encode(gps_msg))
                 self.dri_loc_socket.close()
         except (ConnectionRefusedError, socket.timeout, ConnectionResetError):
-            self.backup_data = msg.data
+            self.backup_data = gps_msg
             self.backup_timer = self.create_timer(
                 2.0, self.backup_caller)
 
