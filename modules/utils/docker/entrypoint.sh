9e0b3bacd5f707a8463ced815f393f4e2fb25bf9
Govind Singh <govind.sk85@gmail.com>
2022-04-27 18:43:55 +0400

    mesh_com: Add provisioning mode clean-up config
    Add args to enter in provisioning mode for
    clean provisioning configuration.
    
    Signed-off-by: Govind Singh <govind.sk85@gmail.com>

--- modules/utils/docker/entrypoint.sh
+++ modules/utils/docker/entrypoint.sh
@@ -19,12 +19,26 @@ calculate_wifi_channel()
     fi
 }
 
-if [[ -z "${1}"  || -z "${2}" ]]; then
+if [[ -z "${1}"  && -z "${2}" ]]; then
     mode="ap+mesh"
     ch="2412"
 else
    mode=$1
    ch=$2
+fi
+
+if [ $mode == "provisioning" ]; then
+   killall hostapd
+   killall wpa_supplicant
+   ifconfig br-lan down
+   brctl delbr br-lan
+   ifname_ap="$(ifconfig -a | grep wlan* | awk -F':' '{ print $1 }')"
+   ifname_mp="$(ifconfig -a | grep wlp1* | awk -F':' '{ print $1 }')"
+   ifconfig $ifname_ap down
+   ifconfig $ifname_mp down
+   ifconfig $ifname_ap up
+   ifconfig $ifname_mp up
+   exit 1
 fi
 
 #start mesh service if mesh provisoning is done
