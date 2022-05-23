332da4cb9b61eacf83c86add8da88392313c9d71
Suhail-SSRC <suhail@ssrc.tii.ae>
2022-05-23 11:27:18 +0400

    pa.conf generic, and removed print statements form main.py in gw
    Signed-off-by: Suhail-SSRC <suhail@ssrc.tii.ae>

--- modules/sc-mesh-secure-deployment/src/gw/main.py
+++ modules/sc-mesh-secure-deployment/src/gw/main.py
@@ -38,18 +38,15 @@ class AutoGateway:
     def clean_and_start(self):
         return_code, null = run_shell_command(
             f"sysctl net.ipv4.ip_forward=1")
-        print("Log 1 for return_code should be !0:" + return_code)
         if return_code != 0:
             self.logger.error("Error setting: sysctl net.ipv4.ip_forward=1")
 
         ret, val = run_shell_command("iptables -t nat -F")
-        print("Log 2 for ret should be !0:" + ret)
         if ret != 0:
             self.logger.error("Error iptables -t nat -F failed")
 
         # delete bat0/br-lan routing
         ret, val = run_shell_command(f"ip route show|grep -e bat0 -e br-lan|grep default|xargs ip route del >/dev/null")
-        print("Log 3 for ret should be !0:" + ret)
         if ret != 0:
             self.logger.error("Error ip route del failed as no previous configuration")
 
@@ -64,7 +61,6 @@ class AutoGateway:
     def find_wwan_iface(self, wifi):
         # use  "$wifi_device" and update wwan_name
         ret, value = run_shell_command("ifconfig")
-        print("Log 4 for ret should be !0:" + ret)
         if ret != 0:
             self.logger.error("Error in ifconfig")
         network_if_list = re.findall(r'(\w+-*\w+):?\s+(?:\bLink\b|\bflags\b)', value)
@@ -81,10 +77,8 @@ class AutoGateway:
 
         for nw_if in network_if_list:
             ret = check_interface_connectivity(nw_if)
-            print("Log 5 for ret should be True:" + ret)
             if ret is True:
                 self.logger.debug(f"internet is available: {nw_if}")
-                print("Log 6 for nw_if should be ?:" + nw_if)
                 return nw_if
             self.logger.debug(f"internet not available {nw_if}")
 
@@ -94,17 +88,14 @@ class AutoGateway:
     def set_local_gateway(self, wwan):
         # todo can this setting be done somewhere else
         ret, val = run_shell_command("iptables -P FORWARD ACCEPT")
-        print("Log 8 for ret should be !0:" + ret)
         if ret != 0:
             self.logger.error("Error in iptables FORWARD ACCEPT")
 
         ret, val = run_shell_command(f"iptables -t nat -A POSTROUTING -o {wwan} -j MASQUERADE")
-        print("Log 9 for ret should be !0:" + ret)
         if ret != 0:
             self.logger.error("Error in adding iptables MASQUERADE")
 
         ret, val = run_shell_command(f"ip route show|grep -e bat0 -e br-lan|grep default|xargs ip route del ")
-        print("Log 10 for ret should be !0:" + ret)
         if ret != 0:
             self.logger.error("Error in ip route del as no previous configuration")
         else:
@@ -113,7 +104,6 @@ class AutoGateway:
     # client
     def configure_mesh_gateway(self, wifi, subnet):
         ret, val = run_shell_command("iptables -t nat -F")
-        print("Log 11 for ret should be !0:" + ret)
         if ret != 0:
             self.logger.error("Error in iptables -t nat -F")
 
@@ -122,10 +112,8 @@ class AutoGateway:
                                              r'"^\*"' +
                                              " | awk '{print $2}'")
         gateway_mac = gateway_mac.strip()
-        print("Log 12 for ret should be !0:" + ret)
         if ret != 0:
             self.logger.error("Error in batctl gwl")
-        print("Log 13 for gateway_mac should be !empty:" + gateway_mac)
         if gateway_mac != "":
             self.logger.debug(f"Gateway available in MAC: {gateway_mac}")
 
@@ -134,7 +122,6 @@ class AutoGateway:
                 self.logger.debug(f"No need to change gateway")
             else:
                 ret2, val2 = run_shell_command(f"ip route del default  >/dev/null")
-                print("Log 14 for ret2 should be !0:" + ret)
                 if ret2 != 0:
                     self.logger.error("Error ip route del failed for default route")
                 # add local default mesh gw
@@ -143,21 +130,18 @@ class AutoGateway:
                 command = "arp-scan " + str(subnet) + " --interface " + wifi + " 2>/dev/null|grep " + gateway_mac + " | awk '{print $1}'"
                 ret, new_gateway_ip = run_shell_command(command)
                 new_gateway_ip = new_gateway_ip.strip()
-                print("Log 15 for ret should be 0:" + ret)
                 if ret == 0:
                     self.logger.debug(
                         f"Gateway available new IP: {new_gateway_ip} old: {self.old_mesh_gateway_mac_and_ip[1]}")
                     # delete old possible local default mesh gw
                     ret, val = run_shell_command(
                         f"ip route del default via {self.old_mesh_gateway_mac_and_ip[1]} >/dev/null")
-                    print("Log 16 for ret should be !0:" + ret)
                     if ret != 0:
                         self.logger.error("Error ip route del failed for old gateway")
                     # add local default mesh gw
                     else:
                         self.logger.debug("Route Gateway Default Route added successfully")
                     ret, val = run_shell_command(f"ip route add default via {new_gateway_ip} >/dev/null")
-                    print("Log 17 for ret should be !0:" + ret)
                     if ret != 0:
                         self.logger.error("Error ip route add failed as no previous configuration")
                     else:
@@ -169,12 +153,10 @@ class AutoGateway:
 
     def find_mesh_ipv4_subnet(self):
         ret, value = run_shell_command("ip -o -f inet addr show | grep -e bat0 -e br-lan | awk '{print $4}'")
-        print("Log 18 for ret should be !0:" + ret)
         if ret != 0:
             self.logger.error("Error ip inet addr show failed")
 
         interface = ipaddress.IPv4Interface(value.strip())
-        print("Log 19 for interface should be some string:" + interface)
         return str(interface.network)
 
     def gateway_client_activity(self):
@@ -185,7 +167,6 @@ class AutoGateway:
         if own_gateway:
             self.logger.debug("change mode to server")
             return_code, null = run_shell_command("batctl gw_mode server")
-            print("Log 20 for return_code should be !0:" + return_code)
             if return_code != 0:
                 self.logger.error("Error in setting gw_mode")
             else:
@@ -202,7 +183,6 @@ class AutoGateway:
         if not own_gateway:
             self.logger.debug("change mode to client")
             return_code, null = run_shell_command("batctl gw_mode client")
-            print("Log 21 for return_code should be !0:" + return_code)
             if return_code != 0:
                 self.logger.error("Error in setting gw_mode")
             else:
@@ -212,7 +192,6 @@ class AutoGateway:
                 return_code, null = run_shell_command(
                     f"iptables -t nat -D POSTROUTING -s {str(self.find_mesh_ipv4_subnet())} "
                     f"-o {self.wan_iface} -j MASQUERADE")
-                print("Log 22 for return_code should be !0:" + return_code)
                 if return_code != 0:
                     self.logger.error("Error deleting MASQUERADE")
                 else:
@@ -222,7 +201,6 @@ class AutoGateway:
                 return_code, null = run_shell_command(
                     f"iptables -t nat -A POSTROUTING -s {str(self.find_mesh_ipv4_subnet())} "
                     f"-o {self.wan_iface} -j MASQUERADE")
-                print("Log 23 for return_code should be !0:" + return_code)
                 if return_code != 0:
                     self.logger.error("Error setting MASQUERADE")
                 else:
