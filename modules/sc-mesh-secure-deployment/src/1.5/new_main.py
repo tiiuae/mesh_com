141fd59662da6bf19a61cc8ecfcf31474ec0af9a
Suhail-SSRC <suhail@ssrc.tii.ae>
2022-06-30 11:29:54 +0400

    Modification of Server and Client in Continous Authentication Features with synchronized Processes/Threads
    Signed-off-by: Suhail-SSRC <suhail@ssrc.tii.ae>

--- modules/sc-mesh-secure-deployment/src/1.5/new_main.py
+++ modules/sc-mesh-secure-deployment/src/1.5/new_main.py
@@ -1,7 +1,11 @@
 import multiprocessing
+from multiprocessing import Lock, Manager
+import functools
 import queue
 import time
 from threading import Thread
+import asyncio
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
 import pandas as pd
 from common import mesh_utils
@@ -16,10 +20,13 @@ import asyncio
 
 
 co = ConnectionMgr.ConnectionMgr()
+_executor = ThreadPoolExecutor()
+executor = ProcessPoolExecutor()
 
 mutual_int = 'wlan1'
 mesh_int = 'bat0'
 client_q = {}
+
 
 
 def launchCA(ID, sectable):  # need to see how to get the IP address of the neighbors
@@ -29,14 +36,40 @@ def launchCA(ID, sectable):  # need to see how to get the IP address of the neig
     '''
     ca = ca_main.CA(ID)
     myip = co.get_ip_address('bat0')
+    max_count = 8
+    flag_ctr = 0
+    #lock = Lock()
     # server_tread = Thread(target=ca.as_server, args=(myip, server_q), daemon=True)
     # server_tread.start()
-    proc = multiprocessing.Process(target=ca.as_server, args=(myip,))
+    proc = multiprocessing.Process(target=ca.as_server, args=(myip,)) #lock variable add later
     proc.start()
+    time.sleep(random.randint(1,10))
+    #proc.join() #This might crash it
+    #with ProcessPoolExecutor() as pool:
+    #        with Manager() as manager:
     for index, row in sectable.iterrows():
-        IP = row['IP']
-        print(IP)
-        client_q[IP] = ca.as_client(IP)
+                                       
+                    manager = multiprocessing.Manager()
+                    return_dict = manager.dict()
+                    jobs = []
+
+                    IP = row['IP']
+                    print(IP)
+
+                    p = multiprocessing.Process(target=ca.as_client, args=(IP, return_dict,))
+                    jobs.append(p)
+                    p.start()
+
+                    for proc in jobs:
+                        proc.join()
+
+                    client_q[IP] = return_dict.values()
+
+                    flag_ctr += 1
+                    if (flag_ctr == max_count):
+                        p.terminate()
+                        break
+                                    
     proc.terminate()
     # server_tread._stop()
     return (client_q)
@@ -46,6 +79,7 @@ def update_table(df,client_q):
     df = df.assign(CA_Result='no')
     for index, row in df.iterrows():
         IP = row['IP']
+        print(f"I AM PRININTG HERE!!!!! {client_q[IP]}")
         df.loc[df['IP'] == IP, 'CA_Result'] = client_q[IP]
     return df
 
@@ -63,15 +97,18 @@ def announcing(message):
 
 
 if __name__ == "__main__":
+#@asyncio.coroutine
+#async def main():
     mut = mutual.Mutual(mutual_int)
     mut.start()
     time.sleep(5)
     if mesh_utils.verify_mesh_status():
         sectable = pd.read_csv('../auth/dev.csv')
         neighbors = mesh_utils.get_mac_mesh(mesh_int)
-        result = launchCA(random.randint(1000, 64000), sectable)
+        result = launchCA(random.randint(1000, 64000), sectable) # result = yield from loop.run_in_executor(executor,launchCA,(random.randint(1000, 64000), sectable))
         sectable = update_table(sectable, result)
 
-
-
-
+#asyncio.run(main())
+#loop = asyncio.get_event_loop()
+#loop.run_until_complete(main())
+#loop.close()
