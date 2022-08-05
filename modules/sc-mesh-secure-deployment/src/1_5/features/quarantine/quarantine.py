9d3f853003d11e693962672190188c9b8e711c5b
mandreoni <jaco@mail.de>
2022-08-05 18:04:39 +0400

    Working with IP blocking * ness_main.py --> added mapping and remapping function to convert nodeID to index. Improved adapt_table function to pass the index instead of nodeID. * new_main.py --> IP is passing when malicious node is detected (line 194) instead of MAC. mapp is received for the adapt_table function on ness * quarantine.py --> now blocking in done on this file. Also, iptables are saved on this file

--- modules/sc-mesh-secure-deployment/src/1_5/features/quarantine/quarantine.py
+++ modules/sc-mesh-secure-deployment/src/1_5/features/quarantine/quarantine.py
@@ -1,20 +1,33 @@
 import subprocess
 import sys
+import time
 
+#script_path = pathlib.Path(__file__).parent.resolve()
 sys.path.insert(0, '../../')
 from common import mesh_utils
 
 
 class Quarantine:
-    def __int__(self):
-        self.interface = mesh_utils.get_mesh_interface()
+    def __init__(self):
+        self.interface = mesh_utils.get_mesh_interface('bat0')
 
-    def block(self, mac):
-        command = ['./traffic_block.sh', mac, self.interface]
+    def block(self, ip):
+        #command = [str(script_path) + '/traffic_block.sh', mac, self.interface]
+        #command = ['iptables', '-A', 'INPUT', '-p', 'ALL', '-m', 'mac', '--mac-source', mac, '-j', 'DROP']
+        command = ['iptables', '-I', 'INPUT', '-p', 'ALL', '-s', ip, '-j', 'DROP']
         subprocess.call(command, shell=False)
-        print(f'blocking MAC: {str(self.mac)} on interface {str(self.interface)}')
+        print(f'blocking IP: {str(ip)} on interface {str(self.interface)}')
+        self.iptablesSave()
 
-    def unblock(self, mac):
-        command = ['./traffic_block.sh', mac, self.interface]
+
+    def unblock(self, ip):
+        #command = [str(script_path) + '/traffic_block.sh', mac, self.interface]
+        #command = ['iptables', '-D', 'INPUT', '-p', 'ALL', '-m', 'mac', '--mac-source', mac, '-j',  'DROP']
+        command = ['iptables', '-D', 'INPUT', '-p', 'ALL', '-s', ip, '-j', 'DROP']
         subprocess.call(command, shell=False)
-        print(f'unblocking MAC: {str(self.mac)} on interface {str(self.interface)}')
+        print(f'unblocking IP: {str(ip)} on interface {str(self.interface)}')
+        self.iptablesSave()
+
+    def iptablesSave(self):
+        command = ['iptables-save', '>', 'iptables-', str(time.time()), '.save']
+        subprocess.call(command, shell=False)
