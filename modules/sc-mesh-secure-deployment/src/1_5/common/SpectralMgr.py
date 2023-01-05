328fb60e85b4ffb1d01b8d67146567c9f2e1fb84
Govind Singh <govind.sk85@gmail.com>
2023-01-05 09:53:48 +0400

    Add csa base class for CSA utility functions
    Add channel switch announcement utility functions.
    
    Signed-off-by: Govind Singh <govind.sk85@gmail.com>

--- modules/sc-mesh-secure-deployment/src/1_5/common/SpectralMgr.py
+++ modules/sc-mesh-secure-deployment/src/1_5/common/SpectralMgr.py
@@ -172,13 +172,4 @@ class Spectral:
     def file_open(fn="data"):
         return open(fn, 'rb')
 
-if __name__ == "__main__":
-    spec = Spectral()
-    spec.initialize_scan()
-    spec.execute_scan()
-    f = spec.file_open("/tmp/data")
-    spec.read(f, 8512)
-    spec.file_close(f)
 
-    print("all values:")
-    print(spec.get_values())
