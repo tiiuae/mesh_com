53e785e14ffcb3d26a7a79245c0ad95e36307756
daniaherzalla <123352270+dania-tii@users.noreply.github.com>
2023-03-02 16:57:19 +0400

    adapt to either driver: ath9k, ath10k

--- modules/sc-mesh-secure-deployment/src/1_5/common/test/spectral_capture.py
+++ modules/sc-mesh-secure-deployment/src/1_5/common/test/spectral_capture.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import pandas as pd
 sys.path.append('../')
 from SpectralMgr import Spectral
 
@@ -10,5 +12,25 @@ if __name__ == "__main__":
     spec.read(f, 8512)
     spec.file_close(f)
 
-    print("all values:")
-    print(spec.get_values())
+    vals_dict = []
+    vals_dict = spec.get_values()
+
+    vals_list = []
+    for key, value in vals_dict.items():
+          vals_list.append(list(value))
+
+
+    driver = os.popen('ls /sys/kernel/debug/ieee80211/phy* | grep ath').read().strip()
+
+    if(driver == "ath9k"):
+        spectral_capture_df = pd.DataFrame(vals_list, columns = ["tsf", "freq", "noise", "rssi", "max_magnitude"])
+        spectral_capture_df = spectral_capture_df.reindex(columns = ["freq1", "freq2", "noise", "max_magnitude",
+                                                                 "total_gain_db", "base_pwr_db", "rssi", "relpwr_db", "avgpwr_db", "tsf"])
+
+    elif(driver == "ath10k"):
+        #spectral_capture_df = pd.DataFrame(vals_list, columns = ["freq1", "freq2", "noise", "max_magnitude",
+        #                                                         "total_gain_db", "base_pwr_db", "rssi", "relpwr_db", "avgpwr_db", "tsf"])
+        spectral_capture_df = pd.DataFrame(vals_list, columns = ["tsf", "freq", "noise", "rssi", "max_magnitude"])
+
+    spectral_capture_df.to_csv('spectral_scan.csv')
+    print(spectral_capture_df)
