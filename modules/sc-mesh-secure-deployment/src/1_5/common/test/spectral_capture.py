a3a73e8f3b59d758d2841d66733e04456032b460
daniaherzalla <123352270+dania-tii@users.noreply.github.com>
2023-03-06 16:50:55 +0400

    Edited features + changed static file reading to dynamic
    Edited script to include necessary features for jamming detection + changed static spectral scan file reading to dynamic

--- modules/sc-mesh-secure-deployment/src/1_5/common/test/spectral_capture.py
+++ modules/sc-mesh-secure-deployment/src/1_5/common/test/spectral_capture.py
@@ -5,32 +5,31 @@ sys.path.append('../')
 from SpectralMgr import Spectral
 
 if __name__ == "__main__":
+
     spec = Spectral()
     spec.initialize_scan()
     spec.execute_scan()
     f = spec.file_open("/tmp/data")
-    spec.read(f, 8512)
+    file_stats = os.stat("/tmp/data")
+    spec.read(f, file_stats.st_size)
     spec.file_close(f)
-
+    
     vals_dict = []
     vals_dict = spec.get_values()
 
     vals_list = []
     for key, value in vals_dict.items():
-          vals_list.append(list(value))
+       vals_list.append(list(value))
 
 
-    driver = os.popen('ls /sys/kernel/debug/ieee80211/phy* | grep ath').read().strip()
+    driver = os.popen('ls /sys/kernel/debug/ieee80211/phy* | grep ath').read().strip()  
 
-    if(driver == "ath9k"):
-        spectral_capture_df = pd.DataFrame(vals_list, columns = ["tsf", "freq", "noise", "rssi", "max_magnitude"])
-        spectral_capture_df = spectral_capture_df.reindex(columns = ["freq1", "freq2", "noise", "max_magnitude",
-                                                                 "total_gain_db", "base_pwr_db", "rssi", "relpwr_db", "avgpwr_db", "tsf"])
+    if(driver == "ath9k"): 
+      spectral_capture_df = pd.DataFrame(vals_list, columns = ["freq1", "noise", "rssi", "signal", "max magnitude"])
+      spectral_capture_df = spectral_capture_df.reindex(columns = ["freq1", "noise","signal", "max_magnitude","total_gain_db", "base_pwr_db", "rssi", "relpwr_db", "avgpwr_db"])
 
     elif(driver == "ath10k"):
-        #spectral_capture_df = pd.DataFrame(vals_list, columns = ["freq1", "freq2", "noise", "max_magnitude",
-        #                                                         "total_gain_db", "base_pwr_db", "rssi", "relpwr_db", "avgpwr_db", "tsf"])
-        spectral_capture_df = pd.DataFrame(vals_list, columns = ["tsf", "freq", "noise", "rssi", "max_magnitude"])
+      spectral_capture_df = pd.DataFrame(vals_list, columns = ["freq1", "noise", "max_magnitude", "total_gain_db","base_pwr_db", "rssi", "relpwr_db", "avgpwr_db"])
 
-    spectral_capture_df.to_csv('spectral_scan.csv')
-    print(spectral_capture_df)
+
+    spectral_capture_df.to_csv('spectral_scan.csv', index=False)
