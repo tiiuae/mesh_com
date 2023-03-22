fccfbc11f94cd3433dfb1bafaf90b31316f0db71
daniaherzalla <123352270+dania-tii@users.noreply.github.com>
2023-03-22 23:03:50 +0400

    integrate scan config file
    moved scan debug mode to SpectralMgr.py read() function

--- modules/sc-mesh-secure-deployment/src/1_5/common/test/spectral_capture.py
+++ modules/sc-mesh-secure-deployment/src/1_5/common/test/spectral_capture.py
@@ -1,35 +1,57 @@
 import sys
 import os
+import yaml
+import subprocess
 import pandas as pd
 sys.path.append('../')
 from SpectralMgr import Spectral
 
 if __name__ == "__main__":
+    count = 0
+    subprocess.call("rm /tmp/data*", shell=True)
 
-    spec = Spectral()
-    spec.initialize_scan()
-    spec.execute_scan()
-    f = spec.file_open("/tmp/data")
-    file_stats = os.stat("/tmp/data")
-    spec.read(f, file_stats.st_size)
-    spec.file_close(f)
+    # SCAN MODE: Check if scan mode arg passed, if not set to default
+    try:        
+        scan_mode = sys.argv[1].strip()
+        if(scan_mode != 'high_latency' and scan_mode != 'low_latency'):
+            scan_mode = 'default'      
+    except:
+        scan_mode = 'default'
     
-    vals_dict = []
-    vals_dict = spec.get_values()
+        
+    # FREQUENCY BAND: Check if band arg passed, if not set to all bands   
+    try:
+        band = sys.argv[2].strip()
+        if(scan_mode == 'high_latency' and (band != 'band_2_4' and band != 'band_5')): # if param passed but invalid input
+            scan_mode = 'default'    
+    except:     
+        scan_mode = 'default'
+        
 
-    vals_list = []
-    for key, value in vals_dict.items():
-       vals_list.append(list(value))
+    # CONFIG
+    with open('spectral_scan_config.yaml') as file:
+      try: 
+        config = yaml.safe_load(file)
+        
+        if(scan_mode == 'low_latency'):
+            scan_channels = config[scan_mode]['channels'][0]
+            all_channels = config[scan_mode]['channels'][0]
+        elif(scan_mode == 'high_latency'):
+            scan_channels = config[scan_mode][band]['channels']
+            all_channels = config[scan_mode][band]['channels']             
+        else:
+            scan_channels = config['default']['channels']
+            all_channels = config['default']['channels']
+      except yaml.YAMLError as exc:
+        print(exc)
 
 
-    driver = os.popen('ls /sys/kernel/debug/ieee80211/phy* | grep ath').read().strip()  
-
-    if(driver == "ath9k"): 
-      spectral_capture_df = pd.DataFrame(vals_list, columns = ["freq1", "noise", "rssi", "signal", "max magnitude"])
-      spectral_capture_df = spectral_capture_df.reindex(columns = ["freq1", "noise","signal", "max_magnitude","total_gain_db", "base_pwr_db", "rssi", "relpwr_db", "avgpwr_db"])
-
-    elif(driver == "ath10k"):
-      spectral_capture_df = pd.DataFrame(vals_list, columns = ["freq1", "noise", "max_magnitude", "total_gain_db","base_pwr_db", "rssi", "relpwr_db", "avgpwr_db"])
-
-
-    spectral_capture_df.to_csv('spectral_scan.csv', index=False)
+    while (True):
+       spec = Spectral()
+       spec.initialize_scan()
+       spec.execute_scan(scan_channels)
+       f = spec.file_open(f"/tmp/data")
+       file_stats = os.stat(f"/tmp/data")
+       scan_channels = spec.read(f, file_stats.st_size, all_channels, scan_channels, count)
+       spec.file_close(f)
+       count += 1
