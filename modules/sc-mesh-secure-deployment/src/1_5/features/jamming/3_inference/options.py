467289511060dfdd347ad0f0d7aec07253afb0df
Dania Herzalla <123352270+dania-tii@users.noreply.github.com>
2023-04-06 17:19:00 +0400

    edit scanning interface

--- modules/sc-mesh-secure-deployment/src/1_5/features/jamming/3_inference/options.py
+++ modules/sc-mesh-secure-deployment/src/1_5/features/jamming/3_inference/options.py
@@ -14,7 +14,7 @@ def parse_option():
     parser.add_argument('--channels2', type=List[int], default=[1, 6, 11], help='Available channels on the 2.4GHz.')
     parser.add_argument('--channels5', type=List[int], default=[36, 40, 44, 48, 149, 153, 157, 161, 165], help='Available channels on the 5GHz.')
     parser.add_argument('--threshold', type=float, default=0.20, help='Threshold on the probability of being jammed.')
-    parser.add_argument('--interface', type=str, default='wlan0', help='Wireless interface name.')
+    parser.add_argument('--interface', type=str, default='wlp1s0', help='Wireless interface name.')
 
     args = parser.parse_args()
 
