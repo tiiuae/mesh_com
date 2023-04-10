7678041cf9792dfb6da2ce04b404d1bb02481e60
Dania Herzalla <123352270+dania-tii@users.noreply.github.com>
2023-04-10 11:02:28 +0400

    Update main.py

--- modules/sc-mesh-secure-deployment/src/1_5/features/jamming/3_inference/main.py
+++ modules/sc-mesh-secure-deployment/src/1_5/features/jamming/3_inference/main.py
@@ -32,49 +32,52 @@ def main():
         # Perform a low latency spectral scan
         channel, scan = scanner.low_latency_spectral_scan()
         if(scan.empty): continue
-        # Preprocess the scan
-        feat_array, frequencies = prep.preprocess(scan)
-        # Estimate the channel quality
-        channel_quality = estimator.estimate(feat_array)
-        # Print channel quality and frequencies
-        print_channel_quality(args, channel_quality, frequencies)
-
-        # If channel quality is below the threshold
-        if channel_quality < args.threshold:
-            # Perform a full scan of the current frequency band
-            channel, scan = scanner.scan_current_band()
-            if(scan.empty): continue
+        else:
             # Preprocess the scan
+            print("no. samples: ", len(scan))
             feat_array, frequencies = prep.preprocess(scan)
+            print('feat_array', feat_array.shape)
             # Estimate the channel quality
-            channels_quality = estimator.estimate(feat_array)
+            channel_quality = estimator.estimate(feat_array)
             # Print channel quality and frequencies
-            print_channel_quality(args, channels_quality, frequencies)
+            print_channel_quality(args, channel_quality, frequencies)
 
-            # Check if there is at least one channel under the threshold
-            if any(quality > args.threshold for quality in channels_quality):
-                # Pick the channel with the highest quality
-                best_freq = frequencies[np.argmax(channels_quality)]
-                # Map the frequency to the corresponding channel
-                best_channel = map_freq_to_channel(best_freq)
-                # Set the device to use the best channel
-                scanner.set_channel(best_channel)
-            else:
-                # Scan the other frequency band
-                channel, scan = scanner.scan_other_band()
+            # If channel quality is below the threshold
+            if channel_quality < args.threshold:
+                # Perform a full scan of the current frequency band
+                channel, scan = scanner.scan_current_band()
+                if(scan.empty): continue
                 # Preprocess the scan
-                feat_array, frequencies_other = prep.preprocess(scan)
-                # Estimate channel quality for the other band
-                channels_quality_other = estimator.estimate(feat_array)
+                feat_array, frequencies = prep.preprocess(scan)
+                # Estimate the channel quality
+                channels_quality = estimator.estimate(feat_array)
                 # Print channel quality and frequencies
-                print_channel_quality(args, channels_quality_other, frequencies_other)
+                print_channel_quality(args, channels_quality, frequencies)
 
-                # Find the best channel between the current and other bands
-                best_quality_both = max(max(channels_quality), max(channels_quality_other))
-                best_freq_both = frequencies[np.argmax(channels_quality)] if best_quality_both in channels_quality else frequencies_other[np.argmax(channels_quality_other)]
-                best_channel_both = map_freq_to_channel(best_freq_both)
-                # Set the device to use the best channel from both bands
-                scanner.set_channel(best_channel_both)
+                # Check if there is at least one channel under the threshold
+                if any(quality > args.threshold for quality in channels_quality):
+                    # Pick the channel with the highest quality
+                    best_freq = frequencies[np.argmax(channels_quality)]
+                    # Map the frequency to the corresponding channel
+                    best_channel = map_freq_to_channel(best_freq)
+                    # Set the device to use the best channel
+                    scanner.set_channel(best_channel)
+                else:
+                    # Scan the other frequency band
+                    channel, scan = scanner.scan_other_band()
+                    # Preprocess the scan
+                    feat_array, frequencies_other = prep.preprocess(scan)
+                    # Estimate channel quality for the other band
+                    channels_quality_other = estimator.estimate(feat_array)
+                    # Print channel quality and frequencies
+                    print_channel_quality(args, channels_quality_other, frequencies_other)
+
+                    # Find the best channel between the current and other bands
+                    best_quality_both = max(max(channels_quality), max(channels_quality_other))
+                    best_freq_both = frequencies[np.argmax(channels_quality)] if best_quality_both in channels_quality else frequencies_other[np.argmax(channels_quality_other)]
+                    best_channel_both = map_freq_to_channel(best_freq_both)
+                    # Set the device to use the best channel from both bands
+                    scanner.set_channel(best_channel_both)
 
         time.sleep(args.waiting_time)
 
