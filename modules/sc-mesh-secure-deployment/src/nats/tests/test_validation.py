7888502b763ca7d3be90b38a381c23a0f79d048a
Mika Joenpera <mika.joenpera@unikie.com>
2023-04-14 08:06:41 +0300

    Add more parameter validation methods and unittests
    test_validation.py - parameter validation unit tests added
    validation.py - parameter validations added
    
    Jira-Id: SPSI-63, SPSI-64
    
    Signed-off-by: Mika Joenpera <mika.joenpera@unikie.com>

--- modules/sc-mesh-secure-deployment/src/nats/tests/test_validation.py
+++ modules/sc-mesh-secure-deployment/src/nats/tests/test_validation.py
@@ -6,10 +6,12 @@ Unittests for validations.py
 import unittest
 from .context import validation
 
+
 class TestValidation(unittest.TestCase):
     """
     Test cases for validations.py
     """
+
     # create test for validate_ssid(ssid)
     def test_validate_ssid(self):
         """
@@ -34,7 +36,8 @@ class TestValidation(unittest.TestCase):
         # psk is invalid (short)
         self.assertFalse(validation.validate_wpa3_psk('1234567'))
         # psk with invalid (long)
-        self.assertFalse(validation.validate_wpa3_psk("""12345678901234567890123456789012345678901234567890123456789012345"""))
+        self.assertFalse(
+            validation.validate_wpa3_psk("""12345678901234567890123456789012345678901234567890123456789012345"""))
 
     def test_validate_ip_address(self):
         """
@@ -59,6 +62,70 @@ class TestValidation(unittest.TestCase):
         self.assertFalse(validation.validate_netmask('0.0.0.255'))
         # netmask is invalid
         self.assertFalse(validation.validate_netmask('a.b.c.d'))
+
+    def test_validate_tx_power(self):
+        """
+        Test cases for validate_tx_power(power_in_dbm)
+        """
+        # power is valid
+        self.assertTrue(validation.validate_tx_power(1))
+        # power is valid
+        self.assertTrue(validation.validate_tx_power(30))
+        # power is invalid
+        self.assertFalse(validation.validate_tx_power(101))
+        # power is invalid
+        self.assertFalse(validation.validate_tx_power(-1))
+
+    def test_validate_country_code(self):
+        """
+        Test cases for validate_country_code(country_code)
+        """
+        # country code is valid
+        self.assertTrue(validation.validate_country_code('DE'))
+        # country code is invalid
+        self.assertFalse(validation.validate_country_code('DEU'))
+        # country code is invalid
+        self.assertFalse(validation.validate_country_code('D'))
+        # country code is invalid
+        self.assertFalse(validation.validate_country_code('DEUTSCHLAND'))
+        # country code is invalid
+        self.assertFalse(validation.validate_country_code(''))
+        # country code is invalid
+        self.assertFalse(validation.validate_country_code(' '))
+        # country code is invalid
+        self.assertFalse(validation.validate_country_code('  '))
+        # country code is invalid
+        self.assertFalse(validation.validate_country_code('   '))
+
+    def test_validate_wifi_mode(self):
+        """
+       Test cases for validate_wifi_mode(mode)
+       """
+        self.assertFalse(validation.validate_mode('ap'))
+        self.assertFalse(validation.validate_mode('sta'))
+        self.assertTrue(validation.validate_mode('mesh'))
+
+    def test_validate_frequency(self):
+        """
+        Test cases for validate_frequency(frequency)
+        """
+        # frequency is valid
+        self.assertTrue(validation.validate_frequency(2412))
+        # frequency is valid
+        self.assertTrue(validation.validate_frequency(2462))
+        # frequency is valid
+        self.assertTrue(validation.validate_frequency(5180))
+        # frequency is valid
+        self.assertTrue(validation.validate_frequency(5825))
+        # frequency is invalid
+        self.assertFalse(validation.validate_frequency(2411))
+        # frequency is invalid
+        self.assertFalse(validation.validate_frequency(2463))
+        # frequency is invalid
+        self.assertFalse(validation.validate_frequency(5179))
+        # frequency is invalid
+        self.assertFalse(validation.validate_frequency(5826))
+
 
 if __name__ == '__main__':
     unittest.main()
--- modules/sc-mesh-secure-deployment/src/nats/validations/validation.py
+++ modules/sc-mesh-secure-deployment/src/nats/validations/validation.py
@@ -1,8 +1,9 @@
 import re
 import socket
+from iso3166 import countries
 
 
-def validate_ssid(ssid):
+def validate_ssid(ssid: str) -> bool:
     """
     Validates a given SSID according to the 802.11 specification.
     Returns True if the SSID is valid, False otherwise.
@@ -13,7 +14,8 @@ def validate_ssid(ssid):
         return False
     return True
 
-def validate_wpa3_psk(psk):
+
+def validate_wpa3_psk(psk: str) -> bool:
     """
     Validates a given PSK for WPA3.
     Returns True if the PSK is valid, False otherwise.
@@ -24,7 +26,8 @@ def validate_wpa3_psk(psk):
         return False
     return True
 
-def validate_ip_address(ip):
+
+def validate_ip_address(ip: str) -> bool:
     """
     Validates a given IP address format.
     Returns True if the IP address is valid, False otherwise.
@@ -32,10 +35,11 @@ def validate_ip_address(ip):
     try:
         socket.inet_pton(socket.AF_INET, ip)
         return True
-    except:
+    except socket.error:
         return False
 
-def validate_netmask(netmask):
+
+def validate_netmask(netmask: str) -> bool:
     """
     Validates a given netmask format.
     Returns True if the netmask is valid, False otherwise.
@@ -51,5 +55,84 @@ def validate_netmask(netmask):
         if '01' in bin_str[1:]:
             return False
         return True
-    except:
+    except (ValueError, TypeError, AttributeError):
         return False
+
+
+def validate_tx_power(power_in_dbm: int) -> bool:
+    """
+    Validates a given TX power in dBm.
+    Returns True if the power is valid, False otherwise.
+    """
+    try:
+        power = int(power_in_dbm)
+        if power < 0 or power > 40:
+            return False
+        return True
+    except (ValueError, NameError, TypeError, AttributeError):
+        return False
+
+
+def validate_country_code(country_code: str) -> bool:
+    """
+    Validates a given country code. Accepts lower and upper case.
+    Returns True if the country alpha2 code is valid, False otherwise.
+    """
+    try:
+        if len(country_code) != 2:
+            return False
+
+        country = countries.get(country_code.upper())
+        if country is None:
+            return False
+        if country.alpha2 != country_code.upper():
+            return False
+        return True
+    except (KeyError, AttributeError, TypeError):
+        return False
+
+
+def validate_mode(mode: str) -> bool:
+    """
+    Validates a given wifi mode.
+    Returns True if the mode is valid, False otherwise.
+    """
+    # todo add correct modes
+    if mode in ["mesh"]:
+        return True
+    return False
+
+
+def validate_frequency(frequency: int) -> bool:
+    """
+    Validates a given wi-fi frequency.
+    Returns True if the frequency is valid, False otherwise.
+    """
+    list_of_2ghz_freq = [2412, 2417, 2422, 2427,
+                         2432, 2437, 2442, 2447,
+                         2452, 2457, 2462, 2467,
+                         2472, 2484]
+    list_of_5ghz_freq = [5180, 5200, 5220, 5240,
+                         5260, 5280, 5300, 5320,
+                         5500, 5520, 5540, 5560,
+                         5580, 5600, 5620, 5640,
+                         5660, 5680, 5700, 5745,
+                         5765, 5785, 5805, 5825]
+    # list_of_6ghz_freq = [5955, 5975, 5995, 6015,
+    #                      6035, 6055, 6075, 6095,
+    #                      6115, 6135, 6155, 6175,
+    #                      6195, 6215, 6235, 6255,
+    #                      6275, 6295, 6315, 6335,
+    #                      6355, 6375, 6395, 6415,
+    #                      6435, 6455, 6475, 6495,
+    #                      6515, 6535, 6555, 6575,
+    #                      6595, 6615, 6635, 6655,
+    #                      6675, 6695, 6715, 6735,
+    #                      6755, 6775, 6795, 6815,
+    #                      6835, 6855, 6875, 6895,
+    #                      6915, 6935, 6955, 6975,
+    #                      6995, 7015, 7035, 7055,
+    #                      7075, 7095, 7115]
+    if frequency in list_of_2ghz_freq or frequency in list_of_5ghz_freq:
+        return True
+    return False
