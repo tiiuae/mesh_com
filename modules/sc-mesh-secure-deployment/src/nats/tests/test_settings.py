ce37a549e17291c57345a71fe4a0edb6b9f93702
Mika Joenpera <mika.joenpera@unikie.com>
2023-04-24 11:17:19 +0300

    Fix settings unitTests
    - class init needs settings and logger object when initialised
    
    Jira-Id: SPSI-63
    
    Signed-off-by: Mika Joenpera <mika.joenpera@unikie.com>

--- modules/sc-mesh-secure-deployment/src/nats/tests/test_settings.py
+++ modules/sc-mesh-secure-deployment/src/nats/tests/test_settings.py
@@ -4,7 +4,10 @@ Unittests for validations.py
 # pylint: disable=import-error, wrong-import-position, unused-import, \
 # disable=unresolved-reference, undefined-variable, too-long
 import unittest
+import logging
 from .context import comms_settings as comms
+from .context import comms_status as comms_status
+
 
 class TestSettings(unittest.TestCase):
     """
@@ -15,7 +18,9 @@ class TestSettings(unittest.TestCase):
         Test cases for handle_mesh_settings()
         """
         # settings json is valid
-        settings = comms.CommsSettings()
+        logger = logging.getLogger("test")
+        cs = comms_status.CommsStatus()
+        settings = comms.CommsSettings(cs, logger)
         ret, info, mesh_status = settings.handle_mesh_settings("""{"api_version": 1,"ssid": "test_mesh", "key": "1234567890","ap_mac": "00:11:22:33:44:55","country": "fi","frequency": "5220","ip": "192.168.1.2","subnet": "255.255.255.0","tx_power": "5","mode": "mesh"}""", "./tests", "test.conf")
         self.assertEqual(ret, "OK", msg=f"ret: {ret}, info: {info}, mesh_status: {mesh_status}")
 
