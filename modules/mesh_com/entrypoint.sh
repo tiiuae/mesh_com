a16061ea704a0242064d42d840c0332a2101b2a7
Matti Paasto <matti.paasto@unikie.com>
2023-09-05 16:18:22 +0300

    Remove obsolete mesh subscriber
    Signed-off-by: Matti Paasto <matti.paasto@unikie.com>

--- modules/mesh_com/entrypoint.sh
+++ modules/mesh_com/entrypoint.sh
@@ -86,7 +86,5 @@ else
 
     # Start mesh publisher
     ros2 run mesh_com mesh_publisher --ros-args -r __ns:=/$DRONE_DEVICE_ID &
-    # Start mesh subscriber
-    ros2 run mesh_com mesh_subscriber --ros-args -r __ns:=/$DRONE_DEVICE_ID
 
 fi
