99e2f380c07868c934d7e844fe8bc84df50f50bf
Anshul Pandey <anshul@ssrc.tii.ae>
2023-11-07 02:34:21 +0400

    Improved script with bug fixes and resolving the issue of infinite execution

--- modules/sc-mesh-secure-deployment/src/2_0/features/PHY/PHY_CRA_scripts/SP_CRA_v7.py
+++ modules/sc-mesh-secure-deployment/src/2_0/features/PHY/PHY_CRA_scripts/SP_CRA_v7.py
@@ -2,47 +2,65 @@ import socket
 import threading
 import random
 import pickle
-#import scipy.io
 from mat4py import loadmat
 import time
 import logging
 import numpy as np
 import os
-from getmac import get_mac_address  # <-- Import the get_mac_address function
+from getmac import get_mac_address
 
 class PHYCRA:
     def __init__(self):
-        logging.basicConfig(level=logging.INFO)
-        logging.getLogger().setLevel(logging.INFO)
-
+        logging.basicConfig(level=logging.INFO, filename='server_log.txt', filemode='w',
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.info("Server initialized")
+        
         # Server setup
-        DataServer = loadmat('ACF_Table.mat')
-        self.acf = np.array(DataServer['y_C']).transpose()
-        #self.acf = DataServer['y_C'].transpose()
-        self.SERVER = self.get_server_ip()
-        self.BROADCAST_PORT = 5051
-        self.PORT = 5050
-        self.ADDR = (self.SERVER, self.PORT)
-        self.FORMAT = 'utf-8'
+        try:
+            DataServer = loadmat('ACF_Table.mat')
+            self.acf = np.array(DataServer['y_C']).transpose()
+            self.SERVER = self.get_server_ip()
+            self.BROADCAST_PORT = 5051
+            self.PORT = 5050
+            self.ADDR = (self.SERVER, self.PORT)
+            self.FORMAT = 'utf-8'
+            logging.info("Server setup completed successfully")
+        except Exception as e:
+            logging.error("Error during server setup: %s", e)
 
         # Client setup
-        DataClient = loadmat('ACF_Table.mat')
-        self.acf_client = np.array(DataClient['y_C']).transpose() 
-        #self.acf_client = DataClient['y_C'].transpose()
+        try:
+            DataClient = loadmat('ACF_Table.mat')
+            self.acf_client = np.array(DataClient['y_C']).transpose()
+            logging.info("Client setup completed successfully")
+        except Exception as e:
+            logging.error("Error during client setup: %s", e)
         
         # Clear the server log at the start of each session
         if os.path.exists("server_log.txt"):
             os.remove("server_log.txt")
 
+        # Create an event to signal when 5 minutes have passed
+        self.stop_event = threading.Event()
+
         # Start server and client functionalities
-        server_thread = threading.Thread(target=self.server_start)
-        server_thread.start()
-        time.sleep(2)
-        listen_thread = threading.Thread(target=self.listen_for_broadcast)
-        listen_thread.start()
+        try:
+            self.server_thread = threading.Thread(target=self.server_start)
+            self.server_thread.start()
+            logging.info("Server thread started")
+            time.sleep(2)
+            self.listen_thread = threading.Thread(target=self.listen_for_broadcast)
+            self.listen_thread.start()
+            logging.info("Broadcast listening thread started")
+        except Exception as e:
+            logging.error("Error during server/client threads initialization: %s", e)
+
+        # Start a timer to stop the server and broadcasting after 5 minutes
+        timer = threading.Timer(60, self.stop_event.set)
+        timer.start()
+        logging.info("Server shutdown timer started")
 
     # SERVER FUNCTIONS
-
     def log_authentication(self, node_ip, mac_address, result):
         timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
         log_entry = f"{timestamp}\t{node_ip}\t{mac_address}\t{result}\n"
@@ -50,6 +68,7 @@ class PHYCRA:
             log_file.write(log_entry)
 
     def display_table(self):
+        logging.info("Displaying authentication table")
         print("+---------------------+---------------+-------------------+---------------------------+")
         print("|        Time         |     Node IP   |    MAC Address    |   Authentication Result   |")
         print("+---------------------+---------------+-------------------+---------------------------+")
@@ -61,47 +80,77 @@ class PHYCRA:
         print("+---------------------+---------------+-------------------+---------------------------+")
 
     def handle_client(self, conn, addr):
+        logging.info(f"Connection request received from {addr}")
         print(f"Connection request received from {addr}")
-        index = random.randint(0, len(self.acf) - 1)
-        acf_tx = pickle.dumps(self.acf[index])
-        conn.send(acf_tx)
-        
-        # Receive and convert rx_index to int
-        rx_index_length = int.from_bytes(conn.recv(2), 'big')
-        rx_index = int(conn.recv(rx_index_length).decode(self.FORMAT))
-        
-        # Then receive MAC address
-        mac_address_length = int.from_bytes(conn.recv(2), 'big')
-        mac_address = conn.recv(mac_address_length).decode(self.FORMAT)
-        
-        if rx_index == index:
-            print("Node is authenticated")
-            self.log_authentication(addr[0], mac_address, "Success")
-        else:
-            print('Access denied')
-            self.log_authentication(addr[0], mac_address, "Access denied")
-        print("\nUpdated Table:")
-        self.display_table()
-        conn.close()
+        try:
+            index = random.randint(0, len(self.acf) - 1)
+            acf_tx = pickle.dumps(self.acf[index])
+            conn.send(acf_tx)
+            
+            # Receive and convert rx_index to int
+            rx_index_length = int.from_bytes(conn.recv(2), 'big')
+            rx_index = int(conn.recv(rx_index_length).decode(self.FORMAT))
+            
+            # Then receive MAC address
+            mac_address_length = int.from_bytes(conn.recv(2), 'big')
+            mac_address = conn.recv(mac_address_length).decode(self.FORMAT)
+            
+            if rx_index == index:
+                print("PASS")
+                logging.info("Authentication successful for %s", addr)
+                self.log_authentication(addr[0], mac_address, "Success")
+            else:
+                print('FAIL')
+                logging.warning("Authentication failed for %s", addr)
+                self.log_authentication(addr[0], mac_address, "Access denied")
+            print("\nUpdated Table:")
+            self.display_table()
+        except Exception as e:
+            logging.error("Error during client handling: %s", e)
+        finally:
+            conn.close()
 
     def server_start(self):
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind(self.ADDR)
-        server.listen()
-        broadcast_thread = threading.Thread(target=self.broadcast_status)
-        broadcast_thread.start()
-        while True:
-            conn, addr = server.accept()
-            thread = threading.Thread(target=self.handle_client, args=(conn, addr))
-            thread.start()
+        try:
+            self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server.bind(self.ADDR)
+            self.server.listen()
+            self.server.settimeout(1)
+            logging.info("Server started and listening")
+        except Exception as e:
+            logging.error("Error during server binding/listening: %s", e)
+            return
+
+        try:
+            self.broadcast_thread = threading.Thread(target=self.broadcast_status)
+            self.broadcast_thread.start()
+            logging.info("Broadcast thread started")
+            while not self.stop_event.is_set():  # Check if the stop event is set
+                try:
+                    conn, addr = self.server.accept()
+                except socket.timeout:
+                    continue
+                thread = threading.Thread(target=self.handle_client, args=(conn, addr))
+                thread.start()
+        except Exception as e:
+            logging.error("Error during server operations: %s", e)
+        finally:
+            self.server.close()
+            logging.info("Server shutdown")
 
     def broadcast_status(self):
-        broadcast_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        broadcast_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        while True:
-            msg = "SERVER_AVAILABLE"
-            broadcast_sock.sendto(msg.encode(), ('<broadcast>', self.BROADCAST_PORT))
-            time.sleep(60)
+        try:
+            broadcast_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            broadcast_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            while not self.stop_event.is_set():  # Check if the stop event is set
+                msg = "SERVER_AVAILABLE"
+                broadcast_sock.sendto(msg.encode(), ('<broadcast>', self.BROADCAST_PORT))
+                time.sleep(30)
+        except Exception as e:
+            logging.error("Error during broadcast: %s", e)
+        finally:
+            broadcast_sock.close()
+            logging.info("Broadcast stopped")
 
     # CLIENT FUNCTIONS
 
@@ -109,24 +158,39 @@ class PHYCRA:
         return get_mac_address()
 
     def connect_to_server(self, server_ip):
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect((server_ip, self.PORT))
-        acf_rx = pickle.loads(client.recv(4096))
-        index = str(np.where((self.acf_client == acf_rx).all(axis=1))[0][0])
-        client.send(len(index).to_bytes(2, 'big'))  # Send length of rx_index first
-        client.send(index.encode(self.FORMAT))  # Send rx_index
-        mac_address = self.get_mac_address()
-        client.send(len(mac_address).to_bytes(2, 'big'))  # Send length of MAC address
-        client.send(mac_address.encode(self.FORMAT))  # Then send MAC address
-        client.close()
+        try:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect((server_ip, self.PORT))
+            acf_rx = pickle.loads(client.recv(4096))
+            index = str(np.where((self.acf_client == acf_rx).all(axis=1))[0][0])
+            client.send(len(index).to_bytes(2, 'big'))  # Send length of rx_index first
+            client.send(index.encode(self.FORMAT))  # Send rx_index
+            mac_address = self.get_mac_address()
+            client.send(len(mac_address).to_bytes(2, 'big'))  # Send length of MAC address
+            client.send(mac_address.encode(self.FORMAT))  # Then send MAC address
+        except Exception as e:
+            logging.error("Error during connection to server: %s", e)
+        finally:
+            client.close()
 
     def listen_for_broadcast(self):
-        listen_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        listen_sock.bind(('', self.BROADCAST_PORT))
-        while True:
-            data, addr = listen_sock.recvfrom(1024)
-            if data.decode() == "SERVER_AVAILABLE" and addr[0] != self.SERVER:
-                self.connect_to_server(addr[0])
+        try:
+            self.listen_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.listen_sock.bind(('', self.BROADCAST_PORT))
+            self.listen_sock.settimeout(1)
+            logging.info("Listening for broadcasts")
+            while not self.stop_event.is_set():
+                try:
+                    data, addr = self.listen_sock.recvfrom(1024)
+                except socket.timeout:
+                    continue
+                if data.decode() == "SERVER_AVAILABLE" and addr[0] != self.SERVER:
+                    self.connect_to_server(addr[0])
+        except Exception as e:
+            logging.error("Error during broadcast reception: %s", e)
+        finally:
+            self.listen_sock.close()
+            logging.info("Stopped listening for broadcasts")
 
     # Common Functions
     def get_server_ip(self):
@@ -141,4 +205,10 @@ class PHYCRA:
         return IP
 
 if __name__ == "__main__":
-    phycra = PHYCRA()
+    phycra_instance = PHYCRA()
+    time.sleep(60)
+    phycra_instance.stop_event.set()
+    phycra_instance.server_thread.join()
+    phycra_instance.listen_thread.join()
+    phycra_instance.broadcast_thread.join()
+
