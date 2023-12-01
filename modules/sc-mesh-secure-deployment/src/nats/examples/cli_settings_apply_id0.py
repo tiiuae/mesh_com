42cd276b0a39f83143863fb60766b795f3cdf7d7
Mika Joenpera <mika.joenpera@unikie.com>
2023-12-01 14:17:50 +0200

    Add functional test code and helpers for CI/CD testing
    Jira-Id: MSS20-219
    
    Signed-off-by: Mika Joenpera <mika.joenpera@unikie.com>

---
+++ modules/sc-mesh-secure-deployment/src/nats/examples/cli_settings_apply_id0.py
@@ -0,0 +1,26 @@
+import asyncio
+import client
+import json
+import config
+
+
+async def main():
+    # Connect to NATS!
+    nc = await client.connect_nats()
+
+    cmd_dict = {"api_version": 1, "cmd": "APPLY", "radio_index": "0"}
+    cmd = json.dumps(cmd_dict)
+    rep = await nc.request(
+        f"comms.command.{config.MODULE_IDENTITY}", cmd.encode(), timeout=15
+    )
+    parameters = json.loads(rep.data)
+    print(json.dumps(parameters, indent=2))
+
+    await nc.close()
+    exit(0)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
