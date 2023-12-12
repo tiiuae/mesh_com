b95019c8540c032e740d6a7de7696ef3d0b2c7ab
Monika-Research <monika.prakash@tii.ae>
2023-12-12 22:28:18 +0400

    Modified the script according to the new format of mptcp config
    Signed-off-by: Monika-Research <monika.prakash@tii.ae>

--- modules/sc-mesh-secure-deployment/src/2_0/features/mptcp/mptcp.sh
+++ modules/sc-mesh-secure-deployment/src/2_0/features/mptcp/mptcp.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 source /var/run/mptcp.conf
 ### MPTCP  ###
-INTERFACE_0=$BRIDGE_IFACE
-for (( i=0; i<=$SUBFLOWS; i++ ))
+iface_list=$(grep 'INTERFACE' /var/run/mptcp.conf)
+i=1;
+while read n;
 do
-   iface=INTERFACE_$i
-   IP=$(ifconfig ${!iface} | grep 'inet' | cut -d: -f2 | awk '{print $2}')
-   NM=$(ifconfig ${!iface} | grep 'netmask' | cut -d: -f2 | awk '{print $4}')
+   iface=$(echo $n | awk -F "=" 'NR==1{print $2}');
+   echo $iface;
+   IP=$(ifconfig $iface | grep 'inet' | cut -d: -f2 | awk '{print $2}')
+   NM=$(ifconfig $iface | grep 'netmask' | cut -d: -f2 | awk '{print $4}')
    IFS=. read -r i1 i2 i3 i4 <<< $IP
    IFS=. read -r m1 m2 m3 m4 <<< $NM
    NP=$(printf "%d.%d.%d.%d\n" "$((i1 & m1))" "$((i2 & m2))" "$((i3 & m3))" "$((i4 & m4))")
-   MASK=$(ip addr show ${!iface} | grep 'inet'| cut -d: -f2 | awk '{print $2}' | awk -F "/" '{print $2}')
+   MASK=$(ip addr show $iface | grep 'inet'| cut -d: -f2 | awk '{print $2}' | awk -F "/" '{print $2}')
 
-   ip rule add from $IP table $((i+1))
-   ip route add $NP/$MASK dev ${!iface} scope link table $((i+1))
+   ip rule add from $IP table $i
+   ip route add $NP/$MASK dev $iface scope link table $i
 
    ip mptcp endpoint add $IP signal
 
-   if [[ $i == 0 ]]; then
-   	BR_NP=$NP
-        BR_MASK=$MASK
+   if  [[ $iface == $BRIDGE_IFACE ]]; then
+   	  BR_NP=$NP
+      BR_MASK=$MASK
    fi
-
-done
-echo $BR_NP
-echo $BR_MASK
+   i=$(($i+1));
+done <<< "$iface_list"
+SUBFLOWS=$i
 ip mptcp limits set subflow $SUBFLOWS add_addr_accepted $SUBFLOWS
 
 BR_PHY=$(brctl show | grep $BRIDGE_IFACE | awk -F " " '{printf $4}')
@@ -39,7 +40,7 @@ iptables -t nat -A PREROUTING -p tcp -j SSREDIR
 iptables -t nat -A SSREDIR -p tcp -d 127.0.0.0/8 -j RETURN
 iptables -t nat -A SSREDIR -p tcp -d 10.0.0.0/8 -j RETURN
 
-iptables -t nat -A SSREDIR -p tcp -s $BR_NP/$BR_MASK -j REDIRECT --to-ports 1080
+iptables -t nat -A SSREDIR -p tcp  -s $BR_NP/$BR_MASK -j REDIRECT --to-ports 1080
 
 ##currently the server ips (end system's ip) are hardcoded here. However with SLAAC this should be fixed
 cat <<EOF > /var/run/ss-redir.json
