52fc2b1e9ffa3ae18a68037452404e59074b9b69
Daniil Troshkov <daniil.troshkov@tii.ae>
2024-02-06 08:54:03 +0400

    cleanup: rm jumming and FMO

--- modules/utils/docker/entrypoint_mdm_agent_utm.sh
+++ modules/utils/docker/entrypoint_mdm_agent_utm.sh
@@ -56,47 +56,4 @@ else
   #######################################
   echo "starting mdm agent for testing purposes"
   /opt/S90mdm_agent start
-
-  # Start jamming service
-  JAMMING=$(extract_features_value "jamming" $YAML_FILE)
-  if [ "$JAMMING" == "true" ]; then
-    echo "starting jamming avoidance service"
-    /opt/S99jammingavoidance start
-  fi
-
-  #######################################
-  # Enable FMO stuff                    #
-  #######################################
-  # FMO can be configured in the features.yaml file
-  FMO=$(extract_features_value "FMO" $YAML_FILE)
-  if [ "$FMO" = "true" ]; then
-    echo "starting Alfred"
-    /opt/S90Alfred start
-
-    echo "starting provisioning agent"
-    # blocks execution until provisioning is done or timeout (30s)
-    # IP address and port are passed as arguments and hardcoded.
-    python /opt/nats/src/comms_provisioning.py -t 30 -s 192.168.1.254 -p 8080 -o /opt > /opt/comms_provisioning.log 2>&1
-
-    echo "Start nats server and client nodes"
-    /opt/S90nats_discovery start
-
-    echo "wait for nats.conf to be created"
-    until [ -f /var/run/nats.conf ]; do
-      sleep 1
-    done
-
-    echo "starting nats server"
-    /opt/S90nats_server start
-
-    echo "starting comms services"
-    /opt/S90comms_controller start
-  fi # FMO
-
-  echo "starting mptcp"
-  if [ -f "/var/run/mptcp.conf" ]; then
-    /opt/S90mptcp start
-  fi
-  # alive
-  nohup /bin/bash -c "while true; do sleep infinity; done"
 fi
