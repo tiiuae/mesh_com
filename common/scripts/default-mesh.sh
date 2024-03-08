80d2318b2c506e5178a1244be15d7964ac430f1d
Sergei Petrov <sergei.petrov@unikie.com>
2024-03-08 13:06:21 +0200

    default-mesh.sh code optimized, duplicated code is now function

--- common/scripts/default-mesh.sh
+++ common/scripts/default-mesh.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -eu
 
 # Check if proper default mesh class is set
 if [[ "$MESH_CLASS" != "gs" && "$MESH_CLASS" != "edge" ]]; then
@@ -8,14 +8,8 @@ else
   echo "Mesh class: " $MESH_CLASS
 fi
 
-# Set IP based on drone role/type.
-if [[ "$DRONE_TYPE" == "recon" ]]; then
-    # 192.168.240.1-192.168.246.255
-    MESH_IP="$EDGE_IP"
-    /opt/ros/${ROS_DISTRO}/share/bin/mesh-11s.sh $MESH_MODE $MESH_IP $MESH_MASK $MESH_MAC $MESH_KEY $MESH_ESSID $MESH_FREQ $MESH_TX $MESH_COUNTRY
-    echo "mesh setup done"
-    gateway_ip="$FOG_GW_VIP" # VRRP FOG virtual IP for the Default mesh
-    route add default gw $gateway_ip bat0
+# Function to check if drone is provisioned
+check_drone_provisioned() {
     echo "INFO: Checking if drone is provisioned..."
     while true; do
         if [ "$DRONE_DEVICE_ID" = "bootstrap" ]; then
@@ -25,6 +19,17 @@ if [[ "$DRONE_TYPE" == "recon" ]]; then
             break
         fi
     done
+}
+
+# Set IP based on drone role/type.
+if [[ "$DRONE_TYPE" == "recon" ]]; then
+    # 192.168.240.1-192.168.246.255
+    MESH_IP="$EDGE_IP"
+    /opt/ros/${ROS_DISTRO}/share/bin/mesh-11s.sh $MESH_MODE $MESH_IP $MESH_MASK $MESH_MAC $MESH_KEY $MESH_ESSID $MESH_FREQ $MESH_TX $MESH_COUNTRY
+    echo "mesh setup done"
+    gateway_ip="$FOG_GW_VIP" # VRRP FOG virtual IP for the Default mesh
+    route add default gw $gateway_ip bat0
+    check_drone_provisioned
     # Start executor. Required to publish ROS2 topic.
     sleep 120
     echo "INFO: Starting ROS topic"
@@ -34,15 +39,7 @@ elif [ "$DRONE_TYPE" == "groundstation" ]; then
     MESH_IP="$GS_IP"
     /opt/ros/${ROS_DISTRO}/share/bin/mesh-11s.sh $MESH_MODE $MESH_IP $MESH_MASK $MESH_MAC $MESH_KEY $MESH_ESSID $MESH_FREQ $MESH_TX $MESH_COUNTRY
     echo "mesh setup done"
-    echo "INFO: Checking if drone is provisioned..."
-    while true; do
-        if [ "$DRONE_DEVICE_ID" = "bootstrap" ]; then
-            sleep 10
-        else
-            echo "INFO: Drone is provisioned, continuing"
-            break
-        fi
-    done
+    check_drone_provisioned
     # Start executor. Required to publish ROS2 topic.
     sleep 120
     echo "INFO: Starting ROS topic"
@@ -61,15 +58,7 @@ elif [[ "$DRONE_TYPE" == "fog" || "$DRONE_TYPE" == "cm-fog" ]]; then
         gateway_ip="$GS_GW_VIP" # VRRP GS virtual IP for the Default mesh
         route add default gw $gateway_ip bat0
     fi
-    echo "INFO: Checking if drone is provisioned..."
-    while true; do
-        if [ "$DRONE_DEVICE_ID" = "bootstrap" ]; then
-            sleep 10
-        else
-            echo "INFO: Drone is provisioned, continuing"
-            break
-        fi
-    done
+    check_drone_provisioned
     # Start executor. Required to publish ROS2 topic.
     sleep 120
     echo "INFO: Starting ROS topic"
@@ -80,18 +69,10 @@ elif [[ "$DRONE_TYPE" == "singlemesh" ]]; then
     MESH_IP="$RECON_GS_IP"
     /opt/ros/${ROS_DISTRO}/share/bin/mesh-11s.sh $MESH_MODE $MESH_IP $MESH_MASK $MESH_MAC $MESH_KEY $MESH_ESSID $MESH_FREQ $MESH_TX $MESH_COUNTRY
     echo "mesh setup done"
-        # mesh class is gs
+    # mesh class is gs
     gateway_ip="$GS_GW_VIP" # VRRP GS virtual IP for the Default mesh
     route add default gw $gateway_ip bat0
-    echo "INFO: Checking if drone is provisioned..."
-    while true; do
-        if [ "$DRONE_DEVICE_ID" = "bootstrap" ]; then
-            sleep 10
-        else
-            echo "INFO: Drone is provisioned, continuing"
-            break
-        fi
-    done
+    check_drone_provisioned
     # Start executor. Required to publish ROS2 topic.
     sleep 120
     echo "INFO: Starting ROS topic"
