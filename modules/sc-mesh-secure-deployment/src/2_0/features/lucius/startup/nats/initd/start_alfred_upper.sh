702d7231f558f349d926465b02f9c76b714c7371
Carlos "' OR 1==1; -- # Ibáñez <carlos.ibanez@tii.ae>
2024-04-01 18:17:15 +0400

    Add changes to all start_alfred_upper.sh files

--- modules/sc-mesh-secure-deployment/src/2_0/features/lucius/startup/nats/initd/start_alfred_upper.sh
+++ modules/sc-mesh-secure-deployment/src/2_0/features/lucius/startup/nats/initd/start_alfred_upper.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-ip link del bat1 2>/dev/null
-ip link add name bat1 type batadv
-batctl meshif bat1 interface create routing_algo BATMAN_V
-ip link set dev bat1 master br-lan
-ip link set dev bat1 up
+echo "wait for bat1 to come up"
+
+tail -F /sys/class/net/bat1/address | awk '{exit}'
 
 /usr/bin/alfred -b bat1 -i bat0 -m -u /var/run/alfred_upper.sock
