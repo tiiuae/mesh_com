9e685f9d0f7188b369698aea252036c8477b864f
Carlos "' OR 1==1; -- # Ibáñez <carlos.ibanez@tii.ae>
2024-04-01 16:59:53 +0400

    Fix bridge bat1 to br-lan instead of other way around in Lucius

--- modules/sc-mesh-secure-deployment/src/nats/lucius/startup/nats/initd/start_alfred_upper.sh
+++ modules/sc-mesh-secure-deployment/src/nats/lucius/startup/nats/initd/start_alfred_upper.sh
@@ -3,7 +3,7 @@
 ip link del bat1 2>/dev/null
 ip link add name bat1 type batadv
 batctl meshif bat1 interface create routing_algo BATMAN_V
-batctl meshif bat1 if add br-lan
+ip link set dev bat1 master br-lan
 ip link set dev bat1 up
 
 /usr/bin/alfred -b bat1 -i bat0 -m -u /var/run/alfred_upper.sock
