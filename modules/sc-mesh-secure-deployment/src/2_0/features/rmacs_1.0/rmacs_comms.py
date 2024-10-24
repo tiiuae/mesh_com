a37620fd8ce43536ce393b909d1df4acec2cb4af
roopa <roopa.shanmugam@unikie.com>
2024-10-24 09:09:58 +0400

    Ported OSF as a part of RMACS control channel along with MR

--- modules/sc-mesh-secure-deployment/src/2_0/features/rmacs_1.0/rmacs_comms.py
+++ modules/sc-mesh-secure-deployment/src/2_0/features/rmacs_1.0/rmacs_comms.py
@@ -9,22 +9,30 @@ import msgpack
 
 
 from config import Config
+from logging_config import logger
 from rmacs_setup import get_mesh_freq, get_ipv6_addr
 # Multicast address and port configuration
-MULTICAST_GROUP = 'ff02::1'  # IPv6 multicast address for entire network
-MULTICAST_PORT = 12345
 
-def rmacs_comms():
+def rmacs_comms(interface):
     """
     Create a RMACS Multicast socket for Server and Client communication
     """
+    #prepare_multicast(interface)
+    #args = Config
+    if interface == 'br-lan':
+        MULTICAST_GROUP = 'ff02::1'
+        MULTICAST_PORT = 12345
+        logger.info(f"MG :{MULTICAST_GROUP}, MP : {MULTICAST_PORT}, interface : {interface}")
+    elif interface == 'osf0':
+        MULTICAST_GROUP = 'ff13::39'
+        MULTICAST_PORT = 12345    
+        logger.info(f"MG :{MULTICAST_GROUP}, MP : {MULTICAST_PORT}")
     try:
         # Create a socket for IPv6 UDP communication
         sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
 
         # Allow multiple sockets to use the same port
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        interface = 'br-lan'
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)    
         sock.bind(('', MULTICAST_PORT))  # '' means bind to all interfaces
 
         # Join the multicast group
@@ -33,10 +41,10 @@ def rmacs_comms():
         sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_IF, interface_index)
 
         # Join the multicast group
-        mreq = struct.pack("16sI", socket.inet_pton(socket.AF_INET6, MULTICAST_GROUP), interface_index)
-        sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
+        rmacs = struct.pack("16sI", socket.inet_pton(socket.AF_INET6, MULTICAST_GROUP), interface_index)
+        sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, rmacs)
 
-        print(f"***++Server listening on {MULTICAST_GROUP}:{MULTICAST_PORT}")
+        logger.info(f"Server listening on {MULTICAST_GROUP}:{MULTICAST_PORT}")
 
         return sock
     except socket.error as sock_err:
@@ -49,15 +57,39 @@ def rmacs_comms():
         logging.error(f"An unexpected error occurred: {ex}")
         return None
     
-def send_data(socket, data):
+def send_data(socket, data, interface) -> None:
+    #prepare_multicast(interface)
+    #args = Config
+    if interface == 'br-lan':
+        MULTICAST_GROUP = 'ff02::1'
+        MULTICAST_PORT = 12345
+        logger.info(f"MG :{MULTICAST_GROUP}, MP : {MULTICAST_PORT}")
+    elif interface == 'osf0':
+        MULTICAST_GROUP = 'ff13::39'
+        MULTICAST_PORT = 12345    
+        logger.info(f"MG :{MULTICAST_GROUP}, MP : {MULTICAST_PORT}")
     try:
         serialized_data = msgpack.packb(data)
         netstring_data = encode(serialized_data)
+        #logger.info(f"MG :{MULTICAST_GROUP}, MP : {MULTICAST_PORT} socket : {socket}, interface = {interface}")
         socket.sendto(netstring_data, (MULTICAST_GROUP, MULTICAST_PORT))  
-        print(f" Send report to Mutlicast")
+        logger.info(f"Send report to Mutlicast")
+        return None
     except BrokenPipeError:
         logging.info(f"Broken pipe error")
     except Exception as e:
         logging.info(f"Error sending data to Multicast")
-        print(f"Error sending data : {e}")
-    pass
+        logger.info(f"Error sending data : {e}")
+
+def prepare_multicast(interface) -> None:
+    
+    if interface == 'br-lan':
+        MULTICAST_GROUP = 'ff02::1'
+        MULTICAST_PORT = 12345
+        logger.info(f"MG :{MULTICAST_GROUP}, MP : {MULTICAST_PORT}, interface : {interface}")
+    elif interface == 'osf0':
+        MULTICAST_GROUP = 'ff13::39'
+        MULTICAST_PORT = 12345    
+        logger.info(f"MG :{MULTICAST_GROUP}, MP : {MULTICAST_PORT}, interface: {interface} ")
+    
+        
